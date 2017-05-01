6d033866bbe4aff985581c50779f22be11596bb7
tingis <tingiskahn@live.se>
2017-05-01 15:57:10 +0200

    Adds a way of simulating the SSM

--- test/timeseries.py
+++ test/timeseries.py
@@ -2,6 +2,7 @@ import unittest
 import pyfilter.timeseries.meta as ts
 import scipy.stats as stats
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def f(x, alpha, sigma):
@@ -21,16 +22,16 @@ def g0(alpha, sigma):
 
 
 class Tests(unittest.TestCase):
-    def test_TimeseriesCreate_1D(self):
-        linear = ts.Base((f0, g0), (f, g), (0.5, 1), (stats.norm, stats.norm))
+    linear = ts.Base((f0, g0), (f, g), (1, 1), (stats.norm, stats.norm))
 
-        assert linear.mean(1) == 0.5 and linear.scale(1) == 1
+    def test_TimeseriesCreate_1D(self):
+
+        assert self.linear.mean(1) == 1 and self.linear.scale(1) == 1
 
     def test_Timeseries2DState(self):
         x = np.random.normal(size=500)
-        linear = ts.Base((f0, g0), (f, g), (0.5, 1), (stats.norm, stats.norm))
 
-        assert np.allclose(linear.mean(x), f(x, 0.5, 1))
+        assert np.allclose(self.linear.mean(x), f(x, 1, 1))
 
     def test_Timeseries3DState2DParam(self):
         alpha = 0.5 * np.ones((500, 1))
@@ -38,3 +39,45 @@ class Tests(unittest.TestCase):
         linear = ts.Base((f0, g0), (f, g), (alpha, 1), (stats.norm, stats.norm))
 
         assert np.allclose(linear.mean(x), f(x, alpha, 1))
+
+    def test_SampleInitial(self):
+        x = self.linear.i_sample()
+
+        assert x is not None
+
+    def test_Propagate(self):
+        out = np.zeros(500)
+
+        out[0] = self.linear.i_sample()
+
+        for i in range(1, out.shape[0]):
+            out[i] = self.linear.propagate(out[i-1])
+
+        plt.plot(out)
+        plt.show()
+
+        assert True
+
+    def test_SampleTrajectory1D(self):
+        sample = self.linear.sample(500)
+
+        assert sample.shape[0] == 500
+
+    def test_SampleTrajectory2D(self):
+        sample = self.linear.sample(500, 250)
+
+        assert sample.shape == (500, 250)
+
+    def test_Weight1D(self):
+        sample = self.linear.i_sample()
+
+        obs = 0
+
+        assert self.linear.weight(obs, sample) == stats.norm.logpdf(obs, loc=sample, scale=1)
+
+    def test_Weight2D(self):
+        sample = self.linear.i_sample(size=(500, 200))
+
+        obs = 0
+
+        assert np.allclose(self.linear.weight(obs, sample), stats.norm.logpdf(obs, loc=sample, scale=1))
