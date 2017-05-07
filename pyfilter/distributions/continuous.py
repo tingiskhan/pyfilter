219578283c517877990735e068c8ae02e90cd890
tingis <tingiskahn@live.se>
2017-05-07 13:43:26 +0200

    Adds distribution module

---
+++ pyfilter/distributions/continuous.py
@@ -0,0 +1,83 @@
+import numpy as np
+from math import pi
+import scipy.stats as stats
+
+
+def _get(x, y):
+    """
+    Returns x if not None, else y
+    :param x:
+    :param y:
+    :return:
+    """
+
+    return x if x is not None else y
+
+
+class Distribution(object):
+    def logpdf(self, *args, **kwargs):
+        """
+        Implements the logarithm of the PDF.
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        raise NotImplementedError()
+
+    def rvs(self, *args, **kwargs):
+        """
+        Samples from the distribution of interest
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        raise NotImplementedError()
+
+    def bounds(self, x):
+        """
+        Checks if bounds are satisfied.
+        :param x:
+        :return:
+        """
+
+        raise NotImplementedError()
+
+
+class Normal(Distribution):
+    def __init__(self, loc=0, scale=1):
+        self.loc = loc
+        self.scale = scale
+
+    def logpdf(self, x, loc=None, scale=None, size=None, **kwargs):
+        m, s = _get(loc, self.loc), _get(scale, self.scale) ** 2
+
+        return - 0.5 * np.log(2 * s * pi) - (x - m) ** 2 / 2 / s
+
+    def rvs(self, loc=None, scale=None, size=None, **kwargs):
+        m, s = _get(loc, self.loc), _get(scale, self.scale)
+
+        return np.random.normal(loc=m, scale=s, size=size)
+
+    def bounds(self, x):
+        return np.abs(x) <= np.infty
+
+
+class Gamma(Distribution):
+    def __init__(self, a, loc=0, scale=1):
+        self.a = a
+        self.loc = loc
+        self.scale = scale
+
+    def logpdf(self, x, a=None, loc=None, scale=None, size=None, **kwargs):
+        a = _get(a, self.a)
+        loc = _get(loc, self.loc)
+        scale = _get(scale, self.scale)
+
+        return stats.gamma.logpdf(x, a=a, loc=loc, scale=scale, size=size, **kwargs)
+
+    def rvs(self, a=None, loc=None, scale=None, size=None, **kwargs):
+        a = _get(a, self.a)
+        loc = _get(loc, self.loc)
+        scale = _get(scale, self.scale)
+
+        return loc + np.random.gamma(a, scale, size=size)
