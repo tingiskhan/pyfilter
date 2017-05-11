5861badc57cf2245a45ceddd38a8a94837e64bb9
tingis <tingiskahn@live.se>
2017-05-11 15:48:49 +0200

    Uses actual market data instead

--- examples/stochvol.py
+++ examples/stochvol.py
@@ -5,6 +5,7 @@ from pyfilter.distributions.continuous import Gamma, Normal
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas_datareader import data
 
 
 def fh0(kappa, gamma, sigma):
@@ -30,24 +31,17 @@ def go(vol, mu):
 def fo(vol, mu):
     return np.exp(vol / 2)
 
-# ===== SIMULATE SSM ===== #
-
-# returns = Base((fh0, gh0), (fh, gh), (0.8, 0, 0.1), (Normal(), Normal()))
-logvol = Base((fh0, gh0), (fh, gh), (0.1, 0, 0.3), (Normal(), Normal()))
-obs = Base((None, None), (go, fo), (0.05,), (None, Normal()))
-
-ssm = StateSpaceModel(logvol, obs)
-
-x, y = ssm.sample(500)
-
-fig, ax = plt.subplots(2)
-ax[0].plot(y)
-ax[1].plot(x)
-
 # ===== INFER VALUES ===== #
 
+fig, ax = plt.subplots(2)
+
+stock = data.DataReader('^gspc', 'yahoo', start='2010-01-01')
+
+y = stock['Adj Close'].pct_change().iloc[1:] * 100
+
+ax[0].plot(y)
+
 logvol = Base((fh0, gh0), (fh, gh), (Gamma(1), Normal(), Gamma(1)), (Normal(), Normal()))
-# returns = Base((fh0, gh0), (fh, gh), (Gamma(1), Normal(), Gamma(1)), (Normal(), Normal()))
 
 obs = Base((None, None), (go, fo), (Normal(),), (None, Normal()))
 
@@ -62,21 +56,16 @@ ax[1].plot(rapf.filtermeans())
 
 # ===== PLOT KDE ===== #
 
-fig2, ax2 = plt.subplots(3)
+fig2, ax2 = plt.subplots(4)
 
+mu = pd.Series(ssm.observable.theta[0])
 kappa = pd.Series(ssm.hidden[0].theta[0])
 gamma = pd.Series(ssm.hidden[0].theta[1])
 sigma = pd.Series(ssm.hidden[0].theta[2])
 
-
-kappa.plot(kind='kde', ax=ax2[0])
-gamma.plot(kind='kde', ax=ax2[1])
-kappa.plot(kind='kde', ax=ax2[2])
-
-# fig3, ax3 = plt.subplots(3)
-#
-# for i, p in enumerate(ssm.hidden[1].theta):
-#     param = pd.Series(p)
-#     param.plot(kind='kde', ax=ax3[i])
+mu.plot(kind='kde', ax=ax2[0])
+kappa.plot(kind='kde', ax=ax2[1])
+gamma.plot(kind='kde', ax=ax2[2])
+sigma.plot(kind='kde', ax=ax2[3])
 
 plt.show()
