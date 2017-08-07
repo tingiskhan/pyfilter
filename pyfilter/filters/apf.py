dbef4395a724e05997229e519f15e697031f00fa
avti01 <victor.tingstrom@ap4.se>
2017-08-07 16:56:20 +0200

    Adds ignore statement

--- pyfilter/filters/apf.py
+++ pyfilter/filters/apf.py
@@ -32,7 +32,8 @@ class APF(BaseFilter):
 
         # ===== Calculate log likelihood ===== #
 
-        self.s_l.append(helps.loglikelihood(self._old_w) + np.log((normalized * np.exp(t_weights)).sum(axis=-1)))
+        with np.errstate(divide='ignore'):
+            self.s_l.append(helps.loglikelihood(self._old_w) + np.log((normalized * np.exp(t_weights)).sum(axis=-1)))
 
         if self.saveall:
             self.s_x.append(t_x)
