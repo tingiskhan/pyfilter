8b343ad41cb2fd0ea7bf2c068a766467e6899945
tingis <tingiskahn@live.se>
2017-10-15 17:19:40 +0200

    Adds numerical gradient and hessian estimation

--- pyfilter/proposals/linearized.py
+++ pyfilter/proposals/linearized.py
@@ -4,65 +4,6 @@ import numpy as np
 from ..utils.utils import dot
 
 
-def _get_derivs(x, func, ndim, h=1e-3):
-    """
-    EStimates the derivative at x of func.
-    :param x:
-    :param func:
-    :param h:
-    :return:
-    """
-
-    if ndim < 2:
-
-        up = x + h
-        low = x - h
-
-        fupx, flowx = func(up), func(low)
-
-        first = (fupx - flowx) / 2 / h
-        second = (fupx - 2 * func(x) + flowx) / h ** 2
-
-    else:
-        first = np.empty_like(x)
-        second = np.zeros((x.shape[0], *x.shape))
-
-        fmid = func(x)
-        derivs = list()
-
-        for i, tx in enumerate(x):
-            up, low = x.copy(), x.copy()
-            up[i] = tx + h
-            low[i] = tx - h
-
-            fupx, flowx = func(up), func(low)
-
-            first[i] = (fupx - flowx) / 2 / h
-            derivs.append((fupx, flowx))
-
-        for i in range(x.shape[0]):
-            for j in range(i, x.shape[0]):
-                if i == j:
-                    second[i, i] = (derivs[i][0] - 2 * fmid + derivs[i][1]) / h ** 2
-                else:
-                    upx, lowx = x.copy(), x.copy()
-
-                    upx[i] += h
-                    upx[j] += h
-
-                    lowx[i] -= h
-                    lowx[j] -= h
-
-                    fupx = func(upx)
-                    flowx = func(lowx)
-
-                    tmp = fupx - derivs[i][0] - derivs[j][0] + 2 * fmid - derivs[i][1] - derivs[j][1] + flowx
-
-                    second[i, j] = second[j, i] = tmp / 2 / h ** 2
-
-    return first, second
-
-
 class Linearized(Proposal):
     def draw(self, y, x, size=None, *args, **kwargs):
         x = self._meaner(x)
@@ -73,9 +14,7 @@ class Linearized(Proposal):
         if self._model.hidden.ndim < 2:
             self._kernel = Normal(mode, np.sqrt(variance))
         else:
-            variances = -np.linalg.inv(second.T).T
-            mean = dot(variances, first)
-            self._kernel = MultivariateNormal(x + mean, np.linalg.cholesky(variances.T).T)
+            self._kernel = MultivariateNormal(mode, np.linalg.cholesky(variance.T).T)
 
         return self._kernel.rvs(size=size)
 
@@ -93,14 +32,24 @@ class Linearized(Proposal):
         while not converged:
             first, second = self._sg.gradient(y, mode, x), self._sg.hess(y, mode, x)
 
-            mode = mode - first / second
+            if self._model.hidden_ndim < 1:
+                mode = mode - first / second
+            else:
+                inversed = np.linalg.inv(second.T).T
+                mode = mode - dot(inversed, first)
+
             if np.all(np.abs(oldmode - mode) < 1e-2) or iters > 3:
                 break
 
             oldmode = mode.copy()
             iters += 1
 
-        return mode, -1 / second
+        if self._model.hidden_ndim < 2:
+            hess = -1 / second
+        else:
+            hess = -inversed
+
+        return mode, hess
 
     def weight(self, y, xn, xo, *args, **kwargs):
         correction = self._kernel.logpdf(xn)
--- pyfilter/utils/stategradient.py
+++ pyfilter/utils/stategradient.py
@@ -1,5 +1,6 @@
 from ..timeseries import StateSpaceModel
 import autograd as ag
+import numpy as np
 
 
 class StateGradient(object):
@@ -72,3 +73,81 @@ class StateGradient(object):
         """
 
         return self._ohess(y, x)
+
+
+class NumericalStateGradient(StateGradient):
+    h = 1e-6
+    grad = None
+
+    def gradient(self, y, x, oldx):
+        """
+        Estimates the gradient numerically.
+        :param y: The observation
+        :param x: The state
+        :param oldx: The previous state
+        :return:
+        """
+
+        if self._model.hidden_ndim < 2:
+            up = x + self.h
+            low = x - self.h
+
+            fupx = self._model.weight(y, up) + self._model.h_weight(up, oldx)
+            flowx = self._model.weight(y, low) + self._model.h_weight(low, oldx)
+
+            self.grad = [flowx, fupx]
+
+            return (fupx - flowx) / 2 / self.h
+
+        grad = np.empty_like(x)
+        self.grad = list()
+        for i, tx in enumerate(x):
+            up, low = x.copy(), x.copy()
+            up[i] = tx + self.h
+            low[i] = tx - self.h
+
+            fupx = self._model.weight(y, up) + self._model.h_weight(up, oldx)
+            flowx = self._model.weight(y, low) + self._model.h_weight(low, oldx)
+
+            grad[i] = (fupx - flowx) / 2 / self.h
+            self.grad.append((fupx, flowx))
+
+        return grad
+
+    def hess(self, y, x, oldx):
+        """
+        Estimates the hessian numerically.
+        :param y: The observation
+        :param x: The state
+        :param oldx: The previous state
+        :return:
+        """
+
+        fx = self._model.weight(y, x) + self._model.h_weight(x, oldx)
+
+        if self._model.hidden_ndim < 2:
+            return (self.grad[1] - 2 * fx + self.grad[0]) / self.h ** 2
+
+        hess = np.empty((x.shape[0], *x.shape))
+        fmid = self._model.weight(y, x) + self._model.h_weight(x, oldx)
+        for i in range(x.shape[0]):
+            for j in range(i, x.shape[0]):
+                if i == j:
+                    hess[i, i] = (self.grad[i][0] - 2 * fmid + self.grad[i][1]) / self.h ** 2
+                else:
+                    upx, lowx = x.copy(), x.copy()
+
+                    upx[i] += self.h
+                    upx[j] += self.h
+
+                    lowx[i] -= self.h
+                    lowx[j] -= self.h
+
+                    fup = self._model.weight(y, upx) + self._model.h_weight(upx, oldx)
+                    flow = self._model.weight(y, lowx) + self._model.h_weight(lowx, oldx)
+
+                    tmp = fup - self.grad[i][0] - self.grad[j][0] + 2 * fmid - self.grad[i][1] - self.grad[j][1] + flow
+
+                    hess[i, j] = hess[j, i] = tmp / 2 / self.h ** 2
+
+        return hess
