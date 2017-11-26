f816485f1cfa48de57603cb8a097afc30d0d93ac
tingis <tingiskahn@live.se>
2017-11-26 14:46:07 +0100

    Improves the RAPF

--- pyfilter/filters/rapf.py
+++ pyfilter/filters/rapf.py
@@ -6,15 +6,16 @@ import numpy as np
 from ..utils.normalization import normalize
 
 
-def _shrink(parameter, shrink):
+def _shrink(parameter, shrink, weights):
     """
     Helper class for shrink parameters
     :param parameter: 
-    :param shrink: 
+    :param shrink:
+    :param weights:
     :return: 
     """
 
-    return shrink * parameter + (1 - shrink) * parameter.mean()
+    return shrink * parameter + (1 - shrink) * np.average(parameter, weights=normalize(weights))
 
 
 def _propose(parameters, indices, h, particles, weights):
@@ -32,8 +33,9 @@ def _propose(parameters, indices, h, particles, weights):
 
     normalized = normalize(weights)
 
-    means = _shrink(params[indices], sqrt(1 - h ** 2))
-    std = h * np.sqrt(np.average((params - params.mean()) ** 2, weights=normalized, axis=0))
+    means = _shrink(params, sqrt(1 - h ** 2), weights)[indices]
+    mean = np.average(params, weights=normalized)
+    std = h * np.sqrt(np.average((params - mean) ** 2, weights=normalized))
 
     a = (bounds[0] - means) / std
     b = (bounds[1] - means) / std
@@ -51,24 +53,33 @@ class RAPF(BaseFilter):
         self.h = sqrt(1 - self.a ** 2)
 
     def filter(self, y):
-        self._copy = self._model.copy()
-        self._copy.p_apply(lambda u: _shrink(u[0], self.a))
+        if not isinstance(self._old_w, np.ndarray):
+            self._old_w = 1 / self._particles * np.ones_like(self._old_x)
 
-        t_x = self._copy.propagate_apf(self._old_x)
-        t_weights = self._copy.weight(y, t_x)
+        # ==== Propagate APF ===== #
+        copy = self._model.copy()
+        t_x = copy.propagate_apf(self._old_x)
+        copy.p_apply(lambda u: _shrink(u[0], self.a, self._old_w))
 
+        # ===== Weight and get indices ===== #
+        t_weights = copy.weight(y, t_x)
         resampled_indices = self._resamp(t_weights + self._old_w)
 
-        self._model.p_apply(lambda u: _propose(u, resampled_indices, self.h, self._p_particles, t_weights))
+        # ===== Propose new parameters ===== #
+        self._model.p_apply(lambda u: _propose(u, resampled_indices, self.h, self._p_particles, self._old_w))
+
+        # ===== Propagate the good states ===== #
 
         resampled_x = choose(self._old_x, resampled_indices)
         x = self._proposal.draw(y, resampled_x)
-        self._old_w = self._proposal.weight(y, x, resampled_x) - choose(t_weights, resampled_indices)
 
+        # ===== Calculate the new weights ===== #
+
+        self._old_w = self._proposal.weight(y, x, resampled_x) - choose(t_weights, resampled_indices)
         self._old_x = x
 
         self.s_l.append(loglikelihood(self._old_w))
-        self.s_mx.append(x.mean(axis=-1))
+        self.s_mx.append(np.sum(x * normalize(self._old_w), axis=-1))
 
         if self.saveall:
             self.s_w.append(self._old_w - choose(t_weights, resampled_indices))
