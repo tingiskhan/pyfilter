02c84f8ab46cc643684ad20c7eb9df1322eb59dd
tingis <tingiskahn@live.se>
2017-11-26 19:14:34 +0100

    Some minor updating

--- examples/stochvol.py
+++ examples/stochvol.py
@@ -1,5 +1,5 @@
 from pyfilter.timeseries import StateSpaceModel, EulerMaruyma, Observable
-from pyfilter.filters import NESSMC2, Linearized
+from pyfilter.filters import NESSMC2, RAPF
 from pyfilter.distributions.continuous import Gamma, Normal
 import autograd.numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +17,6 @@ def gh0(reversion, level, std):
 
 
 def fh(x, reversion, level, std):
-    # return reversion * (level * np.exp(-x) - 1) - std ** 2 / 2
     return reversion * (level - x)
 
 
@@ -25,19 +24,19 @@ def gh(x, reversion, level, std):
     return std
 
 
-def go(vol, level):
-    return level
+def go(vol, level, sigma):
+    return level - sigma ** 2 / 2
 
 
-def fo(vol, level):
-    return np.exp(vol)
+def fo(vol, level, sigma):
+    return np.sqrt(np.exp(vol) + sigma)
 
 
 # ===== GET DATA ===== #
 
 fig, ax = plt.subplots(2)
 
-stock = 'MSFT'
+stock = 'SCHW'
 y = np.log(quandl.get('WIKI/{:s}'.format(stock), start_date='2010-01-01', column_index=11, transform='rdiff', api_key='zJpFs_mvKKNi1-Kse1kx') + 1)
 y *= 100
 
@@ -46,7 +45,7 @@ y *= 100
 
 volparams = Gamma(4, scale=0.1), Normal(0, 1), Gamma(4, scale=0.1)
 logvol = EulerMaruyma((fh0, gh0), (fh, gh), volparams, (Normal(), Normal()))
-obs = Observable((go, fo), (Normal(),), Normal())
+obs = Observable((go, fo), (Normal(), Gamma(4, scale=0.1)), Normal())
 
 ssm = StateSpaceModel(logvol, obs)
 
@@ -76,27 +75,23 @@ ax[0].plot(y.index[-predictions:], ascum.mean(axis=1), color='b', label='Mean')
 actual = y.iloc[-predictions:].cumsum()
 ax[0].plot(y.index[-predictions:], actual, color='g', label='Actual')
 
-ax[1].plot(y.index[:-predictions], np.exp(np.array(alg.filtermeans())))
+ax[1].plot(y.index[:-predictions], np.exp(np.array(alg.filtermeans()) / 2))
 
 plt.legend()
 
 # ===== PLOT KDEs ===== #
 
-fig2, ax2 = plt.subplots(4)
-mu = pd.DataFrame(ssm.observable.theta[0])
-kappa = pd.DataFrame(ssm.hidden.theta[0])
-gamma = pd.DataFrame(ssm.hidden.theta[1])
-sigma = pd.DataFrame(ssm.hidden.theta[2])
+figo, axo = plt.subplots(2)
+for i, p in enumerate(ssm.observable.theta):
+    pd.DataFrame(p).plot(kind='kde', ax=axo[i])
 
-mu.plot(kind='kde', ax=ax2[0])
-kappa.plot(kind='kde', ax=ax2[1])
-gamma.plot(kind='kde', ax=ax2[2])
-sigma.plot(kind='kde', ax=ax2[3])
+figh, axh = plt.subplots(3)
+for i, p in enumerate(ssm.hidden.theta):
+    pd.DataFrame(p).plot(kind='kde', ax=axh[i])
 
+figp, axp = plt.subplots()
 
-fig3, ax3 = plt.subplots()
-
-pd.Series(ascum[-1]).plot(kind='kde', ax=ax3)
-ax3.plot(y.iloc[-predictions:].sum(), 0, 'ro')
+pd.Series(ascum[-1]).plot(kind='kde', ax=axp)
+axp.plot(y.iloc[-predictions:].sum(), 0, 'ro')
 
 plt.show()
