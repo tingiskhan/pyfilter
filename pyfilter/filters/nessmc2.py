0f63566d0f126bfcd35b3e4894c2a5028660dddb
tingis <tingiskahn@live.se>
2018-03-11 22:01:06 +0100

    Adds documentation to NESSMC2

--- pyfilter/filters/nessmc2.py
+++ pyfilter/filters/nessmc2.py
@@ -5,14 +5,26 @@ import pandas as pd
 
 
 class NESSMC2(SMC2):
-    def __init__(self, model, particles, handshake=0.2, **kwargs):
+    def __init__(self, model, particles, handshake=0.2, nesskwargs=None, smc2kwargs=None, **kwargs):
+        """
+        Implements a hybrid of the NESS and SMC2 algorithm, as recommended in the NESS article. That is, we use the
+        SMC2 algorithm for the first part of the series and then switch to NESS when it becomes too computationally
+        demanding to use the SMC2.
+        :param model: The state-space model to filter
+        :type model: pyfilter.timeseries.model.StateSpaceModel
+        :param particles: The number of particles to use targeting (parameters, states)
+        :type particles: tuple of int
+        :param handshake: At which point to switch algorithms, (in percent of length of the series) shoud be <= 1.
+        :type handshake: float
+        :param kwargs: Keyworded arguments used in both algorithms
+        """
         super().__init__(model, particles, **kwargs)
 
         self._hs = handshake
         self._switched = False
 
-        self._smc2 = SMC2(model, particles, **kwargs)
-        self._ness = NESS(model, particles, **kwargs)
+        self._smc2 = SMC2(model, particles, **(smc2kwargs or {}))
+        self._ness = NESS(model, particles, **(nesskwargs or {}))
 
         self._filter = self._ness._filter = self._smc2._filter
 
