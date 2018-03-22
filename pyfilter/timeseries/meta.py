7a317fd68e9a6635f0d2a5dd23f08ed6e953993b
tingis <tingiskahn@live.se>
2018-03-22 19:18:40 +0100

    Improved doc of `meta`

--- pyfilter/timeseries/meta.py
+++ pyfilter/timeseries/meta.py
@@ -8,12 +8,12 @@ class Base(object):
         """
         This object is to serve as a base class for the timeseries models.
         :param initial: The functions governing the initial dynamics of the process
-        :type initial: tuple of functions
+        :type initial: tuple of callable
         :param funcs: The functions governing the dynamics of the process
-        :type funcs: tuple of functions
+        :type funcs: tuple of cabllable
         :param theta: The parameters governing the dynamics
-        :type theta: tuple
-        :param noise: The noise governing the noise process.
+        :type theta: tuple of Distribution|tuple of np.ndarray|float|int
+        :param noise: The noise governing the noise process
         :type noise: tuple of Distribution
         :param q: The correlation of the noise processes
         :type q: numpy.ndarray
@@ -22,15 +22,16 @@ class Base(object):
         self.f0, self.g0 = initial
         self.f, self.g = funcs
         self.theta = theta
-        self._o_theta = theta
+        self._o_theta = theta           # We save the original inputs if is of `Distribution`
         self.noise0, self.noise = noise
         self.q = q
 
     @property
     def ndim(self):
         """
-        Returns the dimension of the timeseries
-        :return:
+        Returns the dimension of the process.
+        :return: Dimension of process
+        :rtype: int
         """
         return self.noise.ndim
 
@@ -38,14 +39,16 @@ class Base(object):
     def priors(self):
         """
         Returns the priors of the time series.
-        :return:
+        :return: The priors of the model
+        :rtype: tuple of Distribution
         """
         return self._o_theta
 
     def i_mean(self):
         """
         Calculates the mean of the initial distribution.
-        :return:
+        :return: The mean of the initial distribution
+        :rtype: np.ndarray|float|int
         """
 
         return resizer(self.f0(*self.theta))
@@ -53,38 +56,48 @@ class Base(object):
     def i_scale(self):
         """
         Calculates the scale of the initial distribution.
-        :return:
+        :return: The scale of the initial distribution
+        :rtype: np.ndarray|float|int
         """
 
         return resizer(self.g0(*self.theta))
 
     def mean(self, x, params=None):
         """
-        Calculates the mean of the process conditional on the state and parameters.
+        Calculates the mean of the process conditional on the previous state and current parameters.
         :param x: The state of the process.
+        :type x: np.ndarray|float|int
         :param params: Used for overriding the parameters
-        :return:
+        :type params: tuple of np.ndarray|float|int
+        :return: The mean
+        :rtype: np.ndarray
         """
 
         return resizer(self.f(x, *(params or self.theta)))
 
     def scale(self, x, params=None):
         """
-        Calculates the scale of the process conditional on the state and parameters.
+        Calculates the scale of the process conditional on the current state and parameters.
         :param x: The state of the process
+        :type x: np.ndarray|float|int
         :param params: Used for overriding the parameters
-        :return:
+        :type params: tuple of np.ndarray|float|int
+        :return: The scale
+        :rtype: np.ndarray|float|int
         """
 
         return resizer(self.g(x, *(params or self.theta)))
 
     def weight(self, y, x, params=None):
         """
-        Weights the process of the current observation x_t with the previous x_{t-1}. Used whenever the proposal
+        Weights the process of the current state `x_t` with the previous `x_{t-1}`. Used whenever the proposal
         distribution is different from the underlying.
         :param y: The value at x_t
+        :type y: np.ndarray|float|int
         :param x: The value at x_{t-1}
-        :param params: The value of the parameters.
+        :type y: np.ndarray|float|int
+        :param params: Used of overriding the parameters
+        :type params: tuple of np.ndarray|float|int
         :return:
         """
 
@@ -94,35 +107,40 @@ class Base(object):
         """
         Samples from the initial distribution.
         :param size: The number of samples
+        :type size: int|tuple of int
         :param kwargs: kwargs passed to the noise class
-        :return:
+        :return: Samples from the initial distribution
+        :rtype: np.ndarray|float|int
         """
 
         return self.noise0.rvs(loc=self.i_mean(), scale=self.i_scale(), size=size, **kwargs)
 
-    def propagate(self, x, **kwargs):
+    def propagate(self, x, params=None):
         """
         Propagates the model forward conditional on the previous state and current parameters.
-        :param x: The previous states
-        :param params: If you wish to override the parameters used for sampling
-        :param kwargs: kwargs passed to the noise class
-        :return:
+        :param x: The previous state
+        :type x: np.ndarray|float|int
+        :param params: Used for overriding the parameters
+        :type params: tuple of np.ndarray|float|int
+        :return: Samples from the model
+        :rtype: np.ndarray|float|int
         """
 
-        loc = self.mean(x, **kwargs)
-        scale = self.scale(x, **kwargs)
+        loc = self.mean(x, params)
+        scale = self.scale(x, params)
 
         return self.noise.rvs(loc=loc, scale=scale)
 
     def sample(self, steps, samples=None, **kwargs):
         """
-        Samples an entire trajectory from the model.
+        Samples a trajectory from the model.
         :param steps: The number of steps
         :type steps: int
-        :param samples: Number of sample paths.
+        :param samples: Number of sample paths
         :type samples: int
-        :param kwargs:
-        :return:
+        :param kwargs: kwargs passed to `i_sample` and `propagate`
+        :return: An array of sampled values
+        :rtype: np.ndarray
         """
 
         out = list()
@@ -135,9 +153,11 @@ class Base(object):
 
     def p_apply(self, func):
         """
-        Applies `func` to each parameter of the model. 
-        :param func: Function to apply, must be of the structure func(param).
-        :return: 
+        Applies `func` to each parameter of the model "inplace", i.e. manipulates `self.theta`.
+        :param func: Function to apply, must be of the structure func(param)
+        :type func: callable
+        :return: Instance of self
+        :rtype: Base
         """
 
         self.theta = self.p_map(func)
@@ -146,8 +166,9 @@ class Base(object):
 
     def p_prior(self):
         """
-        Calculates the prior log likelihood of the current values.
-        :return:
+        Calculates the prior log-likelihood of the current values.
+        :return: The prior of the current parameter values
+        :rtype: np.ndarray|float
         """
 
         return sum(self.p_map(lambda x: x[1].logpdf(x[0]), default=0))
@@ -156,8 +177,11 @@ class Base(object):
         """
         Applies the func to the parameters and returns a tuple of objects.
         :param func: The function to apply to parameters.
-        :param default: What to set those parameters that aren't distributions to. If `None`, sets to the current value.
-        :return:
+        :type func: callable
+        :param default: What to set those parameters that aren't distributions to. If `None`, sets to the current value
+        :type default: np.ndarray|float|int
+        :return: Returns tuple of values
+        :rtype: np.ndarray|float|int
         """
 
         out = tuple()
@@ -172,11 +196,15 @@ class Base(object):
 
     def p_grad(self, y, x, h=1e-3):
         """
-        Calculates the gradient of the model for the current state y with the previous state x, using h as step size.
+        Calculates the gradient of the model parameters for current state `y` with the previous state `x`.
         :param y: The current state
+        :type y: np.ndarray|float|int
         :param x: The previous state
-        :param h: The step size.
-        :return:
+        :type x: np.ndarray|float
+        :param h: The step size
+        :type h: float
+        :return: The gradient of the model parameters
+        :rtype: np.ndarray|float
         """
 
         out = tuple()
