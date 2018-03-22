ba9642cf3e9b405ae0758fae3cc8fcc228dfee8a
tingis <tingiskahn@live.se>
2018-03-22 18:49:00 +0100

    Improved doc of `observable`

--- pyfilter/timeseries/observable.py
+++ pyfilter/timeseries/observable.py
@@ -6,13 +6,11 @@ class Observable(Base):
     def __init__(self, funcs, theta, noise):
         """
         Object for defining the observable part of an HMM.
-        :param funcs: A tuple of functions
-        :type funcs: tuple of functions
         :param funcs: The functions governing the dynamics of the process
-        :type funcs: tuple of functions
+        :type funcs: tuple of callable
         :param theta: The parameters governing the dynamics
-        :type theta: tuple
-        :param noise: The noise governing the noise process.
+        :type theta: tuple of np.ndarray|tuple of float|tuple of Distribution
+        :param noise: The noise governing the noise process
         :type noise: Distribution
         """
         super().__init__((None, None), funcs, theta, (None, noise))
