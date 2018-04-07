d009d487555c28aee48871b51eb214790944b4af
tingis <tingiskahn@live.se>
2018-04-07 15:15:49 +0200

    Condenses calculation

--- pyfilter/filters/nessmc2.py
+++ pyfilter/filters/nessmc2.py
@@ -30,16 +30,14 @@ class NESSMC2(SMC2):
 
     def filter(self, y):
         if self._smc2._ior < self._hs * self._td.shape[0]:
-            self._smc2.filter(y)
-        else:
-            if not self._switched:
-                print('\n===== Switching to NESS =====')
-                self._ness._recw = self._smc2._recw
-                self._switched = True
+            return self._smc2.filter(y)
 
-            self._ness.filter(y)
+        if not self._switched:
+            print('\n===== Switching to NESS =====')
+            self._ness._recw = self._smc2._recw
+            self._switched = True
 
-        return self
+        return self._ness.filter(y)
 
     def longfilter(self, data):
         if isinstance(data, pd.DataFrame):
