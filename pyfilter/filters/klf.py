9495783d0cc7f21d10b3eafebc343187bd07b706
tingis <tingiskahn@live.se>
2018-04-22 13:03:06 +0200

    Adds a semi-working KalmanLaplaceParameters

--- pyfilter/filters/klf.py
+++ pyfilter/filters/klf.py
@@ -33,32 +33,94 @@ class KalmanLaplace(UKF):
 
         return minimize(lambda x: -(self.ssm.weight(y, x) + dist.logpdf(x)), m)
 
-    def filter(self, y):
-        minimized = self._get_x_map(y)
+    def _save(self, y, optstate):
+        """
+        Saves the data.
+        :param optstate: The optimal state
+        :return: Self
+        :rtype: KalmanLaplace
+        """
 
         if self._old_x is None:
-            self._ut.initialize(minimized.x)
+            self._ut.initialize(optstate.x)
 
-        self._ut.xmean = self._old_x = minimized.x.copy()
-        self._ut.xcov = minimized.hess_inv.copy()
+        self._ut.xmean = self._old_x = optstate.x.copy()
+        self._ut.xcov = optstate.hess_inv.copy()
 
-        self.s_mx.append(minimized.x)
+        self.s_mx.append(optstate.x)
         # TODO: Fix this
         self.s_l.append(self.ssm.weight(y, self._old_x))
         self.s_n.append(self._calc_noise(y, self._ut.xmean.copy()))
 
         return self
 
+    def filter(self, y):
+        optstate = self._get_x_map(y)
+
+        return self._save(y, optstate)
+
 
 class KalmanLaplaceParameters(KalmanLaplace):
+    def _params(self, x):
+        """
+        Constructs the parameter space
+        :param x:
+        :return:
+        """
+        obsp = np.array(self.ssm.observable.theta)
+        hidp = np.array(self.ssm.hidden.theta)
 
-    def _opt_params(self, y, x):
+        obsp[self.ssm.ind_obsparams] = x[:len(self.ssm.ind_obsparams)]
+        hidp[self.ssm.ind_hiddenparams] = x[len(self.ssm.ind_obsparams):]
+
+        return obsp, hidp
+
+    def _get_copy(self, p):
+        """
+        Copies current state and overwrites parameters with p.
+        :param p: The paramters to use
+        :type p: np.ndarray
+        :return: Copy of current SSM
+        :rtype: pyfilter.timeseries.model.StateSpaceModel
+        """
+
+        copied = self.ssm.copy()
+
+        obsp, hidp = self._params(p)
+
+        copied.hidden.theta = tuple(hidp.tolist())
+        copied.observable.theta = tuple(obsp.tolist())
+
+        return copied
+
+    def _get_p_map(self, y, x):
         """
         Constructs and performs MAP optimization of the parameters given optimal state.
         :return: The optimization results.
         :rtype: OptimizeResult
         """
 
+        ostart = np.array(self.ssm.observable.theta)[self.ssm.ind_obsparams].tolist()
+        hstart = np.array(self.ssm.hidden.theta)[self.ssm.ind_hiddenparams].tolist()
+
+        obsbounds, hidbounds = self.ssm.optbounds
+
+        if self._old_x is None:
+            def i_func(p):
+                copied = self._get_copy(p)
+                return -(copied.weight(y, x) + copied.hidden.i_weight(x) + copied.p_prior())
+
+            return minimize(i_func, ostart + hstart, bounds=obsbounds + hidbounds)
+
+        # TODO: Fix such that we can perform online optimization. Requires defining artificial dynamics for parameters
+
+        return
+
     def filter(self, y):
         optstate = self._get_x_map(y)
+        params = self._get_p_map(y, optstate.x)
+
+        # TODO: Fix overwriting of parameters
+
+        return self._save(y, optstate)
 
