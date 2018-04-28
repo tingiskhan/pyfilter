4c83247a859295950bcaf471a81da0395e90c9e6
tingis <tingiskahn@live.se>
2018-04-28 12:11:53 +0200

    Removes Parameter-filter (as it's not working) and KLF should handle multi-dimensional timeseries

--- pyfilter/filters/klf.py
+++ pyfilter/filters/klf.py
@@ -22,7 +22,12 @@ class KalmanLaplace(UKF):
         """
 
         if self._old_x is None:
-            return bfgs(lambda x: -(self.ssm.weight(y, x) + self.ssm.hidden.i_weight(x)), self.ssm.hidden.i_mean())
+            start = self.ssm.hidden.i_mean() if self._particles is None else self.ssm.initialize(size=self._p_particles)
+
+            if self.ssm.hidden.ndim < 2 and isinstance(start, np.ndarray):
+                start = start[None]
+
+            return bfgs(lambda x: -(self.ssm.weight(y, x) + self.ssm.hidden.i_weight(x)), start)
 
         spx = self._ut.propagate_sps(only_x=True)
         m, c = _get_meancov(spx, self._ut._wm, self._ut._wc)
@@ -32,7 +37,7 @@ class KalmanLaplace(UKF):
         else:
             dist = MultivariateNormal(m, customcholesky(c))
 
-        return bfgs(lambda x: -(self.ssm.weight(y, x) + dist.logpdf(x)), m)
+        return bfgs(lambda x: -(self.ssm.weight(y, x) + dist.logpdf(x)), m[:, 0])
 
     def _save(self, y, optstate):
         """
@@ -59,99 +64,3 @@ class KalmanLaplace(UKF):
         optstate = self._get_x_map(y)
 
         return self._save(y, optstate)
-
-
-def _define_pdf(params, scale=1e-3):
-    """
-    Helper function for creating the PDF.
-    :param params: The parameters to use for defining the distribution
-    :type params: (np.ndarray, Distribution)
-    :return: A truncated normal distribution
-    :rtype: stats.truncnorm
-    """
-
-    mean = params[0]
-    std = params[1].std() * scale
-
-    a = (params[1].bounds()[0] - mean) / std
-    b = (params[1].bounds()[1] - mean) / std
-
-    return stats.truncnorm(a, b, mean, std)
-
-
-class KalmanLaplaceParameters(KalmanLaplace):
-    """
-    Implements a Kalman-Laplace filter targeting the parameters as well as the state, using artificial dynamics for the
-    parameters.
-    """
-    def _params(self, x):
-        """
-        Constructs the parameter space
-        :param x:
-        :return:
-        """
-        obsp = np.array(self.ssm.observable.theta)
-        hidp = np.array(self.ssm.hidden.theta)
-
-        obsp[self.ssm.ind_obsparams] = x[:len(self.ssm.ind_obsparams)]
-        hidp[self.ssm.ind_hiddenparams] = x[len(self.ssm.ind_obsparams):]
-
-        return obsp, hidp
-
-    def _get_copy(self, p):
-        """
-        Copies current state and overwrites parameters with p.
-        :param p: The paramters to use
-        :type p: np.ndarray
-        :return: Copy of current SSM
-        :rtype: pyfilter.timeseries.model.StateSpaceModel
-        """
-
-        copied = self.ssm.copy()
-
-        obsp, hidp = self._params(p)
-
-        copied.hidden.theta = tuple(hidp.tolist())
-        copied.observable.theta = tuple(obsp.tolist())
-
-        return copied
-
-    def _get_p_map(self, y, x):
-        """
-        Constructs and performs MAP optimization of the parameters given optimal state.
-        :return: The optimization results.
-        :rtype: OptimizeResult
-        """
-
-        ostart = np.array(self.ssm.observable.theta)[self.ssm.ind_obsparams].tolist()
-        hstart = np.array(self.ssm.hidden.theta)[self.ssm.ind_hiddenparams].tolist()
-
-        obsbounds, hidbounds = self.ssm.optbounds
-
-        if self._old_x is None:
-            def i_func(p):
-                copied = self._get_copy(p)
-                return -(copied.weight(y, x) + copied.hidden.i_weight(x) + copied.p_prior())
-
-            return minimize(i_func, ostart + hstart, bounds=obsbounds + hidbounds)
-
-        hdynamics, odynamics = self.ssm.p_map(_define_pdf)
-
-        def s_func(p):
-            copied = self._get_copy(p)
-
-            dynprob = sum(d.logpdf(px) for d, px in zip(odynamics + hdynamics, p.tolist()))
-
-            return -(copied.weight(y, x) + copied.h_weight(x, self._old_x) + dynprob)
-
-        return minimize(s_func, ostart + hstart, bounds=obsbounds + hidbounds)
-
-    def filter(self, y):
-        optstate = self._get_x_map(y)
-        params = self._get_p_map(y, optstate.x)
-
-        self.ssm.observable.theta = tuple(params.x[:len(self.ssm.ind_obsparams)])
-        self.ssm.hidden.theta = tuple(params.x[len(self.ssm.ind_obsparams):])
-
-        return self._save(y, optstate)
-
