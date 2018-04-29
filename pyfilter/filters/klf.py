Fixes loglikehood calculation and adds test

pyfilter/filters/klf.py
@@ -43,8 +43,7 @@ class KalmanLaplace(UKF):
 
             return self._opt(lambda x: -(self.ssm.weight(y, x) + self.ssm.hidden.i_weight(x)), start)
 
-        spx = self._ut.propagate_sps(only_x=True)
-        m, c = _get_meancov(spx, self._ut._wm, self._ut._wc)
+        (m, c, _), _ = self._ut.get_meancov()
 
         if self.ssm.hidden_ndim < 2:
             dist = Normal(m[0], np.sqrt(c[0, 0]))
@@ -64,12 +63,20 @@ class KalmanLaplace(UKF):
         if self._old_x is None:
             self._ut.initialize(optstate.x if self.ssm.hidden_ndim > 1 else optstate.x[0])
 
+        # TODO: Speed this up since we already do the computations earlier
+        _, (ym, yc, _) = self._ut.get_meancov()
+
+        if self.ssm.obs_ndim < 2:
+            dist = Normal(ym[0], np.sqrt(yc[0, 0]))
+        else:
+            dist = MultivariateNormal(ym, customcholesky(yc))
+
         self._ut.xmean = self._old_x = optstate.x
         self._ut.xcov = optstate.hess_inv
 
         self.s_mx.append(optstate.x)
         # TODO: Fix this
-        self.s_l.append(self.ssm.weight(y, self._old_x))
+        self.s_l.append(dist.logpdf(y))
         self.s_n.append(self._calc_noise(y, self._ut.xmean.copy()))
 
         return self

test/filters.py
@@ -354,4 +354,6 @@ class Tests(unittest.TestCase):
 
         rmse = np.sqrt(np.mean((estimates - filterestimates[0]) ** 2))
 
-        assert rmse < 0.05
+        logldiff = np.abs((kf.loglikelihood(y) - np.array(filt.s_l).sum()) / kf.loglikelihood(y))
+
+        assert rmse < 0.05 and logldiff < 0.01