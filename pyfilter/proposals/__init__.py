1edbefba4acb5743b0969b5f747141f03e2a6775
tingis <tingiskahn@live.se>
2018-07-08 13:27:00 +0200

    Updates .gitignore and removes .idea

--- pyfilter/proposals/__init__.py
+++
@@ -1,3 +0,0 @@
-from .bootstrap import Bootstrap
-from .linearized import Linearized
-from .unscented import Unscented, GlobalUnscented
--- pyfilter/timeseries/__init__.py
+++
@@ -1,4 +0,0 @@
-from .observable import Observable
-from .meta import Base
-from .eulermaruyma import EulerMaruyma
-from .model import StateSpaceModel
