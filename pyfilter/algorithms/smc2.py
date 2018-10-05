d7f3ae9e112239762420c059b00e3b45c1f5a984
tingiskhan <victor.tingstrom@ap4.se>
2018-10-05 17:58:39 +0200

    First iteration of SMC2, still needs fixing

--- pyfilter/algorithms/smc2.py
+++ pyfilter/algorithms/smc2.py
@@ -1,8 +1,9 @@
 from .ness import NESS
-from pyfilter.utils.utils import get_ess, expanddims, normalize
+import torch
+from pyfilter.utils.utils import get_ess, add_dimensions, normalize
 import numpy as np
-from pyfilter.distributions.continuous import Distribution, MultivariateNormal
-from pyfilter.filters.base import KalmanFilter
+from ..filters.base import KalmanFilter
+from torch.distributions import MultivariateNormal
 
 
 def _define_pdf(params, weights):
@@ -16,16 +17,16 @@ def _define_pdf(params, weights):
     :rtype: stats.truncnorm
     """
 
-    asarray = np.array([p.t_values for p in params])
+    asarray = torch.cat([p.t_values for p in params], 1)
 
-    if asarray.ndim > 2:
+    if asarray.dim() > 2:
         asarray = asarray[..., 0]
 
-    mean = (asarray * weights).sum(axis=-1)
-    centralized = asarray.T - mean
-    cov = np.einsum('ij,jk->ik', weights * centralized.T, centralized)
+    mean = (asarray * weights[:, None]).sum(0)
+    centralized = asarray - mean
+    cov = torch.einsum('ij,jk->ik', (weights * centralized.t(), centralized))
 
-    return MultivariateNormal(mean, np.linalg.cholesky(cov))
+    return MultivariateNormal(mean, cov)
 
 
 def _mcmc_move(params, dist):
@@ -42,10 +43,10 @@ def _mcmc_move(params, dist):
     if len(shape) > 1:
         shape = shape[:-1]
 
-    rvs = dist.rvs(size=shape)
+    rvs = dist.sample(shape)
 
-    for p, vals in zip(params, rvs):
-        p.t_values = expanddims(vals, p.t_values.ndim)
+    for p, vals in zip(params, rvs.t()):
+        p.t_values = add_dimensions(vals, p.t_values.dim())
 
     return True
 
@@ -61,47 +62,38 @@ def _eval_kernel(params, dist, n_params, n_dist):
     :rtype: np.ndarray
     """
 
-    p_vals = np.array([p.t_values for p in params])
-    n_p_vals = np.array([p.t_values for p in n_params])
+    p_vals = torch.cat([p.t_values for p in params], 1)
+    n_p_vals = torch.cat([p.t_values for p in n_params], 1)
 
-    return n_dist.logpdf(p_vals) - dist.logpdf(n_p_vals)
+    return n_dist.log_prob(p_vals) - dist.log_prob(n_p_vals)
 
 
 class SMC2(NESS):
-    def __init__(self, model, particles, threshold=0.2, disp=False, **filtkwargs):
+    def __init__(self, filter_, particles, threshold=0.2):
         """
         Implements the SMC2 algorithm by Chopin et al.
-        :param model: See BaseFilter
-        :param particles: See BaseFilter
+        :param particles: The amount of particles
+        :type particles: int
         :param threshold: The threshold at which to perform MCMC rejuvenation
         :type threshold: float
-        :param disp: Whether or not to display when the algorithm performs a rejuvenation step
-        :type disp: bool
-        :param filtkwargs: kwargs passed to the filter targeting the states
         """
-        super().__init__(model, particles, **filtkwargs)
+        super().__init__(filter_, particles)
 
         self._th = threshold
-        self._recw = 0      # type: np.ndarray
         self._ior = 0
-        self._disp = disp
+        self._td = tuple()
 
-    def filter(self, y):
-
+    def update(self, y):
         # ===== Perform a filtering move ===== #
-
+        self._td += (y,)
         self._filter.filter(y)
-        self._recw += self._filter.s_l[-1]
+        self._w_rec += self._filter.s_ll[-1]
 
         # ===== Calculate efficient number of samples ===== #
-
-        ess = get_ess(self._recw)
+        ess = get_ess(self._w_rec)
 
         # ===== Rejuvenate if there are too few samples ===== #
-
-        if ess < self._th * self._particles[0]:
-            if self._disp:
-                print('\nESS fell below threshold at index {:d} -> rejuvenating'.format(self._ior))
+        if ess < self._th * self._w_rec.shape[0]:
             self._rejuvenate()
 
         self._ior += 1
@@ -115,23 +107,22 @@ class SMC2(NESS):
         """
 
         # ===== Construct distribution ===== #
-        ll = np.sum(self._filter.s_l, axis=0)
-        dist = _define_pdf(self._filter.ssm.flat_theta_dists, normalize(self._recw))
+        ll = torch.cat(self._filter.s_ll).reshape(self._ior + 1, -1).sum(0)
+        dist = _define_pdf(self._filter.ssm.flat_theta_dists, normalize(self._w_rec))
 
         # ===== Resample among parameters ===== #
-
-        inds = self._resamp(self._recw)
+        inds = self._filter._resamp(self._w_rec)
         self._filter.resample(inds)
 
         # ===== Define new filters and move via MCMC ===== #
 
-        t_filt = self._filter.copy().reset()
+        t_filt = self._filter.copy().reset().initialize()
         _mcmc_move(t_filt.ssm.flat_theta_dists, dist)
 
         # ===== Filter data ===== #
 
-        t_filt.longfilter(self._td[:self._ior+1], bar=False)
-        t_ll = np.sum(t_filt.s_l, axis=0)
+        t_filt.longfilter(np.array(self._td), bar=False)
+        t_ll = torch.cat(t_filt.s_ll).reshape(self._ior + 1, -1).sum(0)
 
         # ===== Calculate acceptance ratio ===== #
         # TODO: Might have to add gradients for transformation?
@@ -141,25 +132,19 @@ class SMC2(NESS):
 
         # ===== Check which to accept ===== #
 
-        u = np.log(np.random.uniform(size=quotient.shape))
-        if plogquot.ndim > 1:
-            toaccept = u < quotient + plogquot[:, 0] + kernel[:, 0]
+        u = np.log(torch.empty(quotient.shape).uniform_())
+        if plogquot.dim() > 1:
+            toaccept = u < quotient + plogquot[:, 0] + kernel
         else:
             toaccept = u < quotient + plogquot + kernel
 
-        if self._disp:
-            print('     Acceptance rate of PMCMC move is {:.1%}'.format(toaccept.mean()))
-
         # ===== Replace old filters with newly accepted ===== #
-
         self._filter.exchange(toaccept, t_filt)
-        self._recw = np.zeros_like(self._recw)
+        self._w_rec = torch.zeros_like(self._w_rec)
 
         # ===== Increase states if less than 20% are accepted ===== #
 
         if toaccept.mean() < 0.2:
-            if self._disp:
-                print('      Acceptance rate fell below threshold - increasing states')
             self._increase_states()
 
         return self
@@ -180,7 +165,7 @@ class SMC2(NESS):
 
         # ===== Calculate new weights and replace filter ===== #
 
-        self._recw = np.sum(t_filt.s_l, axis=0) - np.sum(self._filter.s_l, axis=0)
+        self._w_rec = np.sum(t_filt.s_l, axis=0) - np.sum(self._filter.s_l, axis=0)
         self._filter = t_filt
 
         return self
