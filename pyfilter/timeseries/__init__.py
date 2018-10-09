e3723c5313aeb809695007b00b6ebaf1ba626d2b
tingis <tingiskahn@live.se>
2018-10-09 21:54:36 +0200

    Adds a specialized class for linear Gaussian observations

--- pyfilter/timeseries/__init__.py
+++ pyfilter/timeseries/__init__.py
@@ -2,3 +2,4 @@ from .base import BaseModel, Observable
 from .eulermaruyma import EulerMaruyma
 from .model import StateSpaceModel
 from .parameter import Parameter
+from .linear import LinearGaussianObservations
---
+++ pyfilter/timeseries/linear.py
@@ -0,0 +1,39 @@
+from .model import StateSpaceModel
+from .base import BaseModel, Observable
+import torch
+from torch import distributions as dists
+
+
+def f(x, a, scale):
+    if a.dim() < 2:
+        return a * x
+
+    return torch.einsum('ij,j...->j...', (a, x))
+
+
+def g(x, a, scale):
+    return scale
+
+
+class LinearGaussianObservations(StateSpaceModel):
+    def __init__(self, hidden, a=1., scale=1.):
+        """
+        Implements a State Space model that's linear in the observation equation but has arbitrary dynamics in the
+        state process.
+        :param hidden: The hidden dynamics
+        :type hidden: BaseModel
+        :param a: The A-matrix, must be constant (currently)
+        :type a: torch.Tensor|float
+        :param scale: The variance of the observations, can be constant or learnable. Currently assumes that all
+                      components are independent.
+        :type scale: torch.Tensor|dists.Distribution|float
+        """
+
+        if isinstance(a, float) or a.dim() < 2:
+            noise = dists.Normal(torch.zeros(1), 1)
+        else:
+            noise = dists.Independent(dists.Normal(torch.zeros(a.dim()), 1), 1)
+
+        observable = Observable((f, g), (a, scale), noise)
+
+        super().__init__(hidden, observable)
