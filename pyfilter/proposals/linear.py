2fa4132bd988a52b5e27a6d5e293f004e55116f6
tingis <tingiskahn@live.se>
2018-10-13 13:11:20 +0200

    Updates proposal to simplify adding support for 2D

--- pyfilter/proposals/linear.py
+++ pyfilter/proposals/linear.py
@@ -1,7 +1,6 @@
 from .base import Proposal
-from torch.distributions import Normal
+from torch.distributions import Normal, Independent
 from ..timeseries import LinearGaussianObservations as LGO
-import torch
 
 
 # TODO: Seems to work for 1D models currently, will need to extend to multidimensional
@@ -14,17 +13,35 @@ class LinearGaussianObservations(Proposal):
 
         return self
 
+    @staticmethod
+    def _kernel_1d(y, loc, h_var_inv, o_var_inv, c):
+        cov = 1 / (h_var_inv + c ** 2 * o_var_inv)
+        m = cov * (h_var_inv * loc + c * o_var_inv * y)
+
+        kernel = Normal(m, cov.sqrt())
+
+        return kernel
+
+    def _kernel_2d(self, y, loc, h_var_inv, o_var_inv, c):
+        raise NotImplementedError()
+
     def draw(self, y, x, size=None, *args, **kwargs):
-        h_var = self._model.hidden.scale(x) ** 2
+        # ===== Hidden ===== #
+        loc = self._model.hidden.mean(x)
+        h_var_inv = 1 / (self._model.hidden.scale(x) * self._model.hidden.noise.stddev) ** 2
+
+        # ===== Observable ===== #
         c = self._model.observable.theta_vals[0]
-        o_var = self._model.observable.theta_vals[-1] ** 2
+        o_var_inv = 1 / (self._model.observable.theta_vals[-1] * self._model.observable.noise.stddev) ** 2
 
-        cov = 1 / (1 / h_var + c ** 2 / o_var)
-        m = cov * (1 / h_var * self._model.hidden.mean(x) + c / o_var * y)
-
-        self._kernel = Normal(m, torch.sqrt(cov))
+        if self._model.hidden_ndim < 2:
+            self._kernel = self._kernel_1d(y, loc, h_var_inv, o_var_inv, c)
+        else:
+            self._kernel = self._kernel_2d(y, loc, h_var_inv, o_var_inv, c)
 
         return self._kernel.sample()
 
     def weight(self, y, xn, xo, *args, **kwargs):
-        return self._model.weight(y, xn) + self._model.h_weight(xn, xo) - self._kernel.log_prob(xn)
+        k_prob = self._kernel.log_prob(xn)
+
+        return self._model.weight(y, xn) + self._model.h_weight(xn, xo) - k_prob
