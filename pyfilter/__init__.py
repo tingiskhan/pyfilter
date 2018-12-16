a4253db4b2eaa2cdef89fa85e71c7d0bd9d43984
tingis <tingiskahn@live.se>
2018-12-16 21:19:28 +0100

    __init__ fix

--- pyfilter/__init__.py
+++ pyfilter/__init__.py
@@ -1,6 +1,7 @@
-import torch
-
-USE_CUDA = torch.cuda.is_available()
-
-if USE_CUDA:
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+# TODO: When searchsorted is implemented - add this code
+# import torch
+#
+# USE_CUDA = torch.cuda.is_available()
+#
+# if USE_CUDA:
+#     torch.set_default_tensor_type('torch.cuda.FloatTensor')
