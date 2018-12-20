a51fde5b4eed32eac522d0231b7ad2a8982c5490
tingis <tingiskahn@live.se>
2018-12-20 17:50:21 +0100

    Should be 1

--- pyfilter/proposals/linear.py
+++ pyfilter/proposals/linear.py
@@ -94,7 +94,7 @@ class LinearGaussianObservations(Proposal):
         temp = torch.matmul(tc, h_var.unsqueeze(-1) * tc.t())
         cov = torch.diag(o_var) + temp
 
-        if self._model.obs_ndim > 2:
+        if self._model.obs_ndim > 1:
             return MultivariateNormal(m, scale_tril=torch.cholesky(cov)).log_prob(y)
 
         return Normal(m, cov[..., 0, 0].sqrt()).log_prob(y)
