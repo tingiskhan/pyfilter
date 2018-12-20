3422cd4b009ddd5167204d46373f27db273cc325
tingis <tingiskahn@live.se>
2018-12-20 12:50:56 +0100

    On its way to actually work

--- pyfilter/proposals/linear.py
+++ pyfilter/proposals/linear.py
@@ -1,6 +1,7 @@
 from .base import Proposal
-from torch.distributions import Normal, Independent
+from torch.distributions import Normal, MultivariateNormal, Independent
 from ..timeseries import LinearGaussianObservations as LGO
+import torch
 
 
 # TODO: Seems to work for 1D models currently, will need to extend to multidimensional
@@ -23,16 +24,31 @@ class LinearGaussianObservations(Proposal):
         return kernel
 
     def _kernel_2d(self, y, loc, h_var_inv, o_var_inv, c):
-        raise NotImplementedError()
+        tc = c if c.dim() > 1 else c.unsqueeze(0)
+
+        # ===== Define covariance ===== #
+        t2 = torch.matmul(tc.t(), o_var_inv.unsqueeze(-1) * tc)
+
+        cov = (h_var_inv + t2).inverse()
+
+        # ===== Get mean ===== #
+        t1 = h_var_inv * loc
+
+        t2 = o_var_inv * y
+        t3 = torch.matmul(tc.t(), t2) if t2.dim() > 1 else c * t2
+
+        m = torch.matmul(cov, (t1.unsqueeze(-1) + t3))[..., 0]
+
+        return MultivariateNormal(m, scale_tril=torch.cholesky(cov))
 
     def draw(self, y, x, size=None, *args, **kwargs):
         # ===== Hidden ===== #
         loc = self._model.hidden.mean(x)
-        h_var_inv = 1 / (self._model.hidden.scale(x) * self._model.hidden.noise.stddev) ** 2
+        h_var_inv = 1 / self._model.hidden.scale(x) ** 2
 
         # ===== Observable ===== #
         c = self._model.observable.theta_vals[0]
-        o_var_inv = 1 / (self._model.observable.theta_vals[-1] * self._model.observable.noise.stddev) ** 2
+        o_var_inv = 1 / self._model.observable.scale(x) ** 2
 
         if self._model.hidden_ndim < 2:
             kernel = self._kernel_1d(y, loc, h_var_inv, o_var_inv, c)
@@ -42,12 +58,19 @@ class LinearGaussianObservations(Proposal):
         return kernel.sample()
 
     def weight(self, y, xn, xo, *args, **kwargs):
-        if self._model.hidden_ndim < 2:
-            c = self._model.observable.theta_vals[0]
-            m = c * self._model.hidden.mean(xo)
+        c = self._model.observable.theta_vals[0]
 
-            std = (self._model.hidden.scale(xo) ** 2 + (c * self._model.observable.scale(xo)) ** 2).sqrt()
+        h_var = self._model.hidden.scale(xo) ** 2
+        o_var = self._model.observable.scale(xo) ** 2
+
+        m = self._model.observable.mean(xo)
+
+        if self._model.hidden_ndim < 2:
+            std = (h_var + c ** 2 * o_var).sqrt()
 
             return Normal(m, std).log_prob(y)
 
-        raise NotImplementedError()
+        temp = torch.matmul(c, o_var.unsqueeze(-1) * c.t())
+        cov = h_var + temp
+
+        return MultivariateNormal(m, scale_tril=torch.cholesky(cov)).log_prob(y)
