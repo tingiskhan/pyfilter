4a8506a1b58380f74383cb1685866f03a1c7f503
tingis <tingiskahn@live.se>
2019-01-05 16:56:12 +0100

    Better EulerMaruyma

--- pyfilter/timeseries/eulermaruyma.py
+++ pyfilter/timeseries/eulermaruyma.py
@@ -1,20 +1,23 @@
 from .base import BaseModel
 import torch
+from torch.distributions import Normal, Independent
 
 
 class EulerMaruyma(BaseModel):
-    def __init__(self, initial, funcs, theta, noise, dt=1.):
+    def __init__(self, initial, funcs, theta, dt=1., ndim=1):
         """
         Implements the Euler-Maruyama scheme.
-        :param initial: See BaseModel
-        :param funcs: See BaseModel
-        :param theta: See BaseModel
-        :param noise: See BaseModel
+        :param ndim: The number of dimensions
+        :type ndim: int
         :param dt: The step-size to use in the approximation. If `dt=1`, is basically AR process
         :type dt: float
         """
 
-        super().__init__(initial, funcs, theta, noise)
+        dist = Normal(torch.zeros(ndim), torch.ones(ndim))
+        if ndim > 1:
+            dist = Independent(dist, 0)
+
+        super().__init__(initial, funcs, theta, (dist, dist))
         self.dt = torch.tensor(float(dt)) if not isinstance(dt, torch.Tensor) else dt
         self._sqdt = self.dt.sqrt()
 
