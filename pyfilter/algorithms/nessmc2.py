06e5ae84aa152f07c3431693a673b14516edbf61
tingis <tingiskahn@live.se>
2019-01-10 14:56:14 +0100

    Uses same iterator

--- pyfilter/algorithms/nessmc2.py
+++ pyfilter/algorithms/nessmc2.py
@@ -1,5 +1,6 @@
 from .smc2 import SMC2
 from .ness import NESS
+from tqdm import tqdm
 
 
 class NESSMC2(SMC2):
@@ -25,13 +26,23 @@ class NESSMC2(SMC2):
         self._smc2.initialize()
         return self
 
+    def fit(self, y):
+        self._iterator = self._smc2._iterator = self._ness._iterator = tqdm(y, desc=str(self))
+
+        for yt in self._iterator:
+            self.update(yt)
+
+        return self
+
     def update(self, y):
         if len(self._smc2._y) < self._hs:
             return self._smc2.update(y)
 
         if not self._switched:
             self._switched = True
-            self._filter = self._ness._filter = self._smc2._filter
+            self.filter = self._ness.filter = self._smc2.filter
             self._ness._w_rec = self._smc2._w_rec
+
+            self._iterator.set_description(desc=str(self))
 
         return self._ness.update(y)
