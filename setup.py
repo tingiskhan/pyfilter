065bdbdd0686de444c5bc3cea96f6b7ad35edccc
tingis <tingiskahn@live.se>
2019-01-15 08:32:04 +0100

    Updates setup

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     description='Package for performing online Bayesian inference in state space models',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1.11.3',
         'matplotlib>=2.0.0',
         'pandas>=0.19.2',
         'scipy>=0.18.1',
         'torch>=0.4.1',
-        'tqdm>=4.26'
+        'tqdm>=4.26',
+        'scikit-learn>=0.20.1',
+        'pytorch>=1.0.0'
     ]
 )
