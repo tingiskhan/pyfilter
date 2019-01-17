59c4e2f159f2c117faf9e69ad6de2f11724ced45
tingis <tingiskahn@live.se>
2019-01-17 19:16:39 +0100

    Updates setup

--- setup.py
+++ setup.py
@@ -12,9 +12,8 @@ setup(
         'matplotlib>=2.0.0',
         'pandas>=0.19.2',
         'scipy>=0.18.1',
-        'torch>=0.4.1',
+        'torch>=1.0.0',
         'tqdm>=4.26',
         'scikit-learn>=0.20.1',
-        'pytorch>=1.0.0'
     ]
 )
