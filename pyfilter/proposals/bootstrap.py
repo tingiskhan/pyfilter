2633f6d6354a513a886d445ef0ab0f3816cb1e7e
tingis <tingiskahn@live.se>
2019-01-19 17:01:28 +0100

    Fixes bootstrap

--- pyfilter/proposals/bootstrap.py
+++ pyfilter/proposals/bootstrap.py
@@ -5,8 +5,13 @@ class Bootstrap(Proposal):
     """
     Implements the Bootstrap proposal. I.e. sampling from the prior distribution.
     """
-    def draw(self, y, x):
-        return self._model.propagate(x)
+
+    def construct(self, y, x):
+        self._kernel = lambda: self._model.hidden.propagate(x)
+        return self
+
+    def draw(self):
+        return self._kernel()
 
     def weight(self, y, xn, xo):
         return self._model.weight(y, xn)
