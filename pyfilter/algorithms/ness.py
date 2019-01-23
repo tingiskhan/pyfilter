c0e8e628dcb00ba9770b7fd26032ca9d9225b8c4
tingis <tingiskahn@live.se>
2019-01-23 11:26:53 +0100

    Doc update and shrinkage now function of `p`

--- pyfilter/algorithms/ness.py
+++ pyfilter/algorithms/ness.py
@@ -23,13 +23,15 @@ def cont_jitter(params, scale):
     return values + scale * torch.empty_like(values).normal_()
 
 
-def shrinkage_jitter(parameter, w):
+def shrinkage_jitter(parameter, w, p):
     """
     Jitters the parameters using the optimal shrinkage of ...
     :param parameter: The parameters of the model, inputs as (values, prior)
     :type parameter: Parameter
     :param w: The normalized weights
     :type w: torch.Tensor
+    :param p: The p parameter
+    :type p: float
     :return: Proposed values
     :rtype: torch.Tensor
     """
@@ -47,7 +49,7 @@ def shrinkage_jitter(parameter, w):
 
     var = std ** 2
 
-    bw = 1.59 * std * ess ** (-1 / 3)
+    bw = 1.59 * std * ess ** (-p / (p + 2))
 
     beta = ((var - bw ** 2) / var).sqrt()
 
@@ -70,11 +72,11 @@ def disc_jitter(parameter, i, w):
     if i.sum() == 0:
         return parameter.t_values
 
-    return (1 - i) * parameter.t_values + i * shrinkage_jitter(parameter, w)
+    return (1 - i) * parameter.t_values + i * shrinkage_jitter(parameter, w, 1)
 
 
 class NESS(SequentialAlgorithm):
-    def __init__(self, filter_, particles, threshold=1., continuous=True, resampling=systematic, shrink=True, p=4):
+    def __init__(self, filter_, particles, threshold=1., continuous=True, resampling=systematic, shrink=True, p=1.):
         """
         Implements the NESS alorithm by Miguez and Crisan.
         :param particles: The particles to use for approximating the density
@@ -83,7 +85,9 @@ class NESS(SequentialAlgorithm):
         :type threshold: float
         :param continuous: Whether to use continuous or discrete jittering
         :type continuous: bool
-        :param p: For controlling the variance of the jittering kernel. The greater the value, the higher the variance.
+        :param p: For controlling the variance of the jittering kernel. The greater the value, the higher the variance
+        for `shrink=False`. When `shrink=True`, `p` controls the amount of shrinkage applied. The smaller the value the
+        more shrinkage is applied. Note that `p=1` is recommendended when using `continuous=False`.
         """
 
         super().__init__(filter_)
@@ -103,7 +107,7 @@ class NESS(SequentialAlgorithm):
         # TODO: Need to figure out why kernels aren't working too good...
         if continuous:
             if shrink:
-                self.kernel = shrinkage_jitter
+                self.kernel = lambda u, w: shrinkage_jitter(u, w, p=p)
             else:
                 scale = 1 / math.sqrt(particles ** ((p + 2) / p))
                 self.kernel = lambda u, w: cont_jitter(u, scale)
