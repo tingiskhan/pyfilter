9916edbe45b8f45a629216b3f3f5c11e71179024
tingis <tingiskahn@live.se>
2019-02-05 20:14:46 +0100

    Reverts to old behavior

--- pyfilter/algorithms/ness.py
+++ pyfilter/algorithms/ness.py
@@ -220,16 +220,11 @@ class NESS(SequentialAlgorithm):
         self._logged_ess += (self._ess,)
 
         # ===== Jitter ===== #
-        glob_ess = self._ess
-        if isinstance(self.filter, ParticleFilter):
-            # TODO: Not too sure about this, but think it's correct
-            glob_ess = get_ess((self._w_rec[:, None] + self.filter._w_old).view(-1))
-
         if self.kernel is disc_jitter:
             i = torch.empty(self._shape).bernoulli_(1 / self._ess ** (self._p / 2))
-            f = lambda x: self.kernel(x, i, normalize(self._w_rec), self._p, glob_ess, self._shrink)
+            f = lambda x: self.kernel(x, i, normalize(self._w_rec), self._p, self._ess, self._shrink)
         else:
-            f = lambda x: self.kernel(x, normalize(self._w_rec), self._p, glob_ess, self._shrink)
+            f = lambda x: self.kernel(x, normalize(self._w_rec), self._p, self._ess, self._shrink)
 
         self.filter.ssm.p_apply(f, transformed=True)
 
