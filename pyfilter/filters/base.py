9379fc1304f4a8db6d92f1d9e543f7eee2c43a2a
Victor <victor.gruselius@gmail.com>
2019-08-22 13:15:44 +0200

    Changes resampling to multinomial

--- pyfilter/filters/base.py
+++ pyfilter/filters/base.py
@@ -327,7 +327,7 @@ def cudawarning(resampling):
 
 
 class ParticleFilter(BaseFilter):
-    def __init__(self, model, particles, resampling=systematic, proposal='auto', ess=0.9, need_grad=False):
+    def __init__(self, model, particles, resampling=multinomial, proposal='auto', ess=0.9, need_grad=False):
         """
         Implements the base functionality of a particle filter.
         :param particles: How many particles to use
