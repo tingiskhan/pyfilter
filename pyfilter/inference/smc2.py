b959074300b737482801c60dae881244186cf6a9
tingis <tingiskahn@live.se>
2020-03-11 14:15:45 +0100

    Bug fix

--- pyfilter/inference/smc2.py
+++ pyfilter/inference/smc2.py
@@ -139,7 +139,7 @@ class SMC2FW(SequentialParticleAlgorithm):
             self._iterator.set_description(str(self))
 
         # ===== Check if to propagate ===== #
-        force_rejuv = self._logged_ess[-1] < 0.1 * self._particles or (~torch.isfinite(self._w_rec)).any()
+        force_rejuv = self._logged_ess[-1] < 0.1 * self._particles[0] or (~torch.isfinite(self._w_rec)).any()
         if self._num_iters % self._bl == 0 or force_rejuv:
             self._kernel.update(self.filter.ssm.theta_dists, self.filter, self._w_rec)
             self._num_iters = 0
