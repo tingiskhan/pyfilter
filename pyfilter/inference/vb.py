fc6c6ffadaf5774d200746adf589cdbc1d027dfa
Victor <victor.gruselius@gmail.com>
2020-04-30 17:25:48 +0200

    Use filter for initializing state approximation for quicker conv

--- pyfilter/inference/vb.py
+++ pyfilter/inference/vb.py
@@ -1,37 +1,38 @@
 from .base import BatchAlgorithm
 import torch
-from torch import optim
+from torch.optim import Adam
 import tqdm
 from .varapprox import StateMeanField, BaseApproximation, ParameterMeanField
 from ..timeseries import StateSpaceModel
 from ..timeseries.base import StochasticProcess
 from .utils import stacker
 from ..utils import EPS, unflattify
+from ..filters import SISR
 
 
 # TODO: Shape not working correctly when transformed != untransformed
 class VariationalBayes(BatchAlgorithm):
-    def __init__(self, model, num_samples=4, approx=None, optimizer=optim.Adam, maxiters=30e3, optkwargs=None):
+    def __init__(self, model, samples=4, approx=None, optimizer=Adam, maxiter=30e3, optkwargs=None, use_filter=True):
         """
         Implements Variational Bayes for stochastic processes implementing either `StateSpaceModel` or
         `StochasticProcess`.
         :param model: The model
         :type model: StateSpaceModel|StochasticProcess
-        :param num_samples: The number of samples
-        :type num_samples: int
+        :param samples: The number of samples
+        :type samples: int
         :param approx: The variational approximation to use for the latent space
         :type approx: BaseApproximation
         :param optimizer: The optimizer
         :type optimizer: optim.Optimizer
-        :param maxiters: The maximum number of iterations
-        :type maxiters: int|float
+        :param maxiter: The maximum number of iterations
+        :type maxiter: int|float
         :param optkwargs: Any optimizer specific kwargs
         :type optkwargs: dict
         """
 
         super().__init__()
         self._model = model
-        self._numsamples = num_samples
+        self._ns = samples
 
         # ===== Approximations ===== #
         self._is_ssm = isinstance(model, StateSpaceModel)
@@ -46,10 +47,11 @@ class VariationalBayes(BatchAlgorithm):
         self._mask = None
         self._runavg = 0.
         self._decay = 0.975
+        self._use_filter = use_filter
 
         # ===== Optimization stuff ===== #
         self._opt_type = optimizer
-        self._maxiters = int(maxiters)
+        self._maxiters = int(maxiter)
         self.optkwargs = optkwargs or dict()
 
     @property
@@ -77,18 +79,18 @@ class VariationalBayes(BatchAlgorithm):
         :rtype: VariationalBayes
         """
 
-        params = self._p_approx.sample(self._numsamples)
+        params = self._p_approx.sample(self._ns)
         for p, msk in zip(self._model.theta_dists, self._mask):
             p.detach_()
             p[:] = unflattify(p.bijection(params[:, msk]), p.c_shape)
 
-        self._model.viewify_params((self._numsamples, 1))
+        self._model.viewify_params((self._ns, 1))
 
         return self
 
     def _initialize(self, y):
         # ===== Sample model in place for a primitive version of initialization ===== #
-        self._model.sample_params(self._numsamples)
+        self._model.sample_params(self._ns)
         self._mask = stacker(self._model.theta_dists).mask    # NB: We create a mask once
 
         # ===== Setup the parameter approximation ===== #
@@ -98,6 +100,16 @@ class VariationalBayes(BatchAlgorithm):
         # ===== Initialize the state approximation ===== #
         if self._is_ssm:
             self._s_approx.initialize(y)
+
+            # ===== Run filter and use means for initialization ====== #
+            if self._use_filter:
+                filt = SISR(self._model.copy(), 1000).viewify_params((self._ns, 1)).set_nparallel(self._ns)
+                filt.initialize().longfilter(y, bar=False)
+
+                maxind = filt.result.loglikelihood.sum(0).argmax()
+                self._s_approx._mean.data[1:] = filt.result.filter_means[:, maxind]
+
+            # ===== Append parameters ===== #
             params += self._s_approx.get_parameters()
 
         return params
@@ -113,7 +125,7 @@ class VariationalBayes(BatchAlgorithm):
 
         if self._is_ssm:
             # ===== Sample states ===== #
-            transformed = self._s_approx.sample(self._numsamples)
+            transformed = self._s_approx.sample(self._ns)
 
             # ===== Helpers ===== #
             x_t = transformed[:, 1:]
