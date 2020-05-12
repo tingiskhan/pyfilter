2b24aee6c52d69241547d0bc4d7701241b47be7b
tingis <tingiskahn@live.se>
2020-05-12 19:53:31 +0200

    APF bug fix

--- pyfilter/filters/apf.py
+++ pyfilter/filters/apf.py
@@ -11,19 +11,22 @@ class APF(ParticleFilter):
 
     def _filter(self, y):
         # ===== Perform auxiliary sampling ===== #
-        self.proposal.construct(y, self._x_cur)
         pre_weights = self.proposal.pre_weight(y, self._x_cur)
 
         resamp_w = pre_weights + self._w_old
         normalized = normalize(self._w_old)
 
-        # ===== Resample and propagate ===== #
+        # ===== Resample ===== #
         resampled_indices = self._resampler(resamp_w)
         resampled_x = choose(self._x_cur, resampled_indices)
 
-        self._proposal = self.proposal.resample(resampled_indices)
-        self._x_cur = self._proposal.draw(self._rsample)
+        self.proposal.resample(resampled_indices)
 
+        # ===== Construct ===== #
+        self.proposal.construct(y, resampled_x)
+
+        # ===== Propagate and weight ===== #
+        self._x_cur = self._proposal.draw(self._rsample)
         weights = self.proposal.weight(y, self._x_cur, resampled_x)
 
         self._w_old = weights - choose(pre_weights, resampled_indices)
