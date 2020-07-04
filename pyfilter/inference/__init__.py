bc6f57930d39c3defb4e3015b0ce7c11467232a9
tingis <tingiskahn@live.se>
2020-07-04 16:18:28 +0200

    Better structure, and enhanced logging

--- pyfilter/inference/__init__.py
+++ pyfilter/inference/__init__.py
@@ -2,5 +2,4 @@ from .ness import NESS
 from .nessmc2 import NESSMC2
 from .smc2 import SMC2
 from .smc2fw import SMC2FW
-from .iteratedfiltering import IteratedFilteringV2
 from .vb import VariationalBayes
