c4703d7f502a002ceb59967f14ed890e4693219e
Victor <victor.gruselius@gmail.com>
2020-07-29 18:06:44 +0200

    Removes locals

--- pyfilter/timeseries/models/ou.py
+++ pyfilter/timeseries/models/ou.py
@@ -13,15 +13,16 @@ class OrnsteinUhlenbeck(AffineProcess):
         :param ndim: The number of dimensions for the Brownian motion
         """
 
-        def f(x: torch.Tensor, reversion: object, level: object, std: object):
-            return level + (x - level) * torch.exp(-reversion * dt)
-
-        def g(x: torch.Tensor, reversion: object, level: object, std: object):
-            return std / (2 * reversion).sqrt() * (1 - torch.exp(-2 * reversion * dt)).sqrt()
-
         if ndim > 1:
             dist = Independent(Normal(torch.zeros(ndim), torch.ones(ndim)), 1)
         else:
             dist = Normal(0., 1)
 
-        super().__init__((f, g), (kappa, gamma, sigma), dist, dist)
+        super().__init__((self._f, self._g), (kappa, gamma, sigma), dist, dist)
+        self._dt = torch.tensor(dt)
+
+    def _f(self, x, k, g, s):
+        return g + (x - g) * torch.exp(-k * self._dt)
+
+    def _g(self, x, k, g, s):
+        return s / (2 * k).sqrt() * (1 - torch.exp(-2 * k * self._dt)).sqrt()
