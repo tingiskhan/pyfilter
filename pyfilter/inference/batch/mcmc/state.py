9d4af2ce11d877d54364ff86c655998a62aaf001
tingis <tingiskahn@live.se>
2020-12-17 08:24:34 +0100

    PMMH fix

--- pyfilter/inference/batch/mcmc/state.py
+++ pyfilter/inference/batch/mcmc/state.py
@@ -1,15 +1,17 @@
 from ...state import AlgorithmState
 import torch
 from ....filters import FilterResult
+from ....utils import TensorList
 
 
 class PMMHState(AlgorithmState):
     def __init__(self, initial_sample: torch.Tensor, filter_result: FilterResult):
-        self.samples = [initial_sample]
+        super().__init__()
+        self.samples = TensorList(initial_sample)
         self.filter_result = filter_result
 
     def update(self, sample: torch.Tensor):
         self.samples.append(sample)
 
     def as_tensor(self):
-        return torch.stack(self.samples)
+        return torch.stack(self.samples.values(), 0)
