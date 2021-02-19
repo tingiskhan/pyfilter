bf3eec2d8a6902d0487355cc1f813d8836fc10aa
tingis <victor.tingstrom@gmail.com>
2021-02-19 18:26:24 +0100

    Direct import

--- pyfilter/typing.py
+++ pyfilter/typing.py
@@ -3,10 +3,10 @@ import numpy as np
 from typing import Union, Tuple, Callable
 import torch
 from .distributions import Prior
+from .timeseries.state import TimeseriesState
 
-
-StateLike = Union[Distribution, "TimeseriesState", Prior]
+StateLike = Union[Distribution, TimeseriesState, Prior]
 ArrayType = Union[float, int, np.ndarray, StateLike]
 ShapeLike = Union[int, Tuple[int, ...], torch.Size]
 MeanOrScaleFun = Callable[[StateLike, Tuple[torch.Tensor, ...]], torch.Tensor]
-DiffusionFunction = Callable[["TimeseriesState", float, Tuple[torch.Tensor, ...]], Distribution]
+DiffusionFunction = Callable[[TimeseriesState, float, Tuple[torch.Tensor, ...]], Distribution]
