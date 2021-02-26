239b37add6ee575f2c383faa78f98ba63f726e56
tingis <victor.tingstrom@gmail.com>
2021-02-26 11:06:24 +0100

    Adds prior

--- pyfilter/distributions/typing.py
+++ pyfilter/distributions/typing.py
@@ -1,6 +1,7 @@
 from torch.distributions import Distribution
 import torch
-from typing import Type, Callable, Sequence, Union, Dict
+from typing import Type, Callable, Sequence, Union
+from .prior import Prior
 
 
 DistributionOrBuilder = Union[
@@ -8,4 +9,4 @@ DistributionOrBuilder = Union[
     Callable[[Type[Distribution], Sequence[Union[torch.Tensor, float]]], Distribution]
 ]
 
-Parameters = Union[torch.Tensor, float, int]
+Parameters = Union[torch.Tensor, float, int, Prior]
