95c2f7147a3bea12bf6c7b5cfd892c510b15d552
tingis <victor.tingstrom@gmail.com>
2021-04-03 17:28:34 +0200

    Moves validate_args to parameters instead

--- pyfilter/distributions/parameterized.py
+++ pyfilter/distributions/parameterized.py
@@ -11,20 +11,14 @@ class DistributionWrapper(BuilderMixin, PriorModule):
     Implements a wrapper around pytorch distributions in order to enable moving distributions between devices.
     """
 
-    def __init__(self, base_dist: DistributionOrBuilder, validate_args=False, **parameters: Union[Parameters, Prior]):
+    def __init__(self, base_dist: DistributionOrBuilder, **parameters: Union[Parameters, Prior]):
         super().__init__()
 
         self.base_dist = base_dist
-        self._validate_args = validate_args
+        parameters["validate_args"] = parameters.pop("validate_args", False)
 
         for k, v in parameters.items():
             if isinstance(v, Prior):
                 self.register_prior(k, v)
             else:
                 self.register_buffer(k, v if isinstance(v, torch.Tensor) else torch.tensor(v))
-
-    def build_distribution(self):
-        return self.base_dist(**self.get_parameters(), validate_args=self._validate_args)
-
-    def forward(self):
-        return self.build_distribution()
