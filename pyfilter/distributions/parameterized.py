54064844066a4d6cb4915c10e5f8467bacc6ff8c
tingis <victor.tingstrom@gmail.com>
2021-04-03 10:01:16 +0200

    Skips validating args as we allow nans

--- pyfilter/distributions/parameterized.py
+++ pyfilter/distributions/parameterized.py
@@ -7,13 +7,24 @@ from .typing import DistributionOrBuilder, Parameters
 
 
 class DistributionWrapper(BuilderMixin, PriorModule):
-    def __init__(self, base_dist: DistributionOrBuilder, **parameters: Union[Parameters, Prior]):
+    """
+    Implements a wrapper around pytorch distributions in order to enable moving distributions between devices.
+    """
+
+    def __init__(self, base_dist: DistributionOrBuilder, validate_args=False, **parameters: Union[Parameters, Prior]):
         super().__init__()
 
         self.base_dist = base_dist
+        self._validate_args = validate_args
 
         for k, v in parameters.items():
             if isinstance(v, Prior):
                 self.register_prior(k, v)
             else:
                 self.register_buffer(k, v if isinstance(v, torch.Tensor) else torch.tensor(v))
+
+    def build_distribution(self):
+        return self.base_dist(**self.get_parameters(), validate_args=self._validate_args)
+
+    def forward(self):
+        return self.build_distribution()
