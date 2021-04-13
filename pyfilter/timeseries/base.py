91fca0e0798137e007110ab7f3ddd4137c25398b
tingis <victor.tingstrom@gmail.com>
2021-04-13 19:42:20 +0200

    Adds initial stuff

--- pyfilter/timeseries/base.py
+++ pyfilter/timeseries/base.py
@@ -1,11 +1,12 @@
 from torch.distributions import Distribution
 import torch
 from copy import deepcopy
-from typing import Tuple, Union, TypeVar, Callable
+from typing import TypeVar, Callable
 from torch.nn import Module
-from ..distributions import PriorMixin
+from ..distributions import PriorMixin, DistributionWrapper
 from .state import TimeseriesState
 from ..typing import ShapeLike
+from ..utils import size_getter
 
 
 T = TypeVar("T")
@@ -14,18 +15,44 @@ T = TypeVar("T")
 # TODO: Rename this to process and remove process.py
 class Base(PriorMixin, Module):
     """
-    Defines the base class for the timeseries suite.
+    Defines the base class for stochastic processes
     """
 
-    def __init__(self):
+    def __init__(self, initial_dist: DistributionWrapper):
         super().__init__()
+        self.inital_dist = initial_dist
         self._post_process_state: Callable[[TimeseriesState, TimeseriesState], None] = None
 
-    def parameters_to_array(self, transformed=False, as_tuple=False) -> torch.Tensor:
-        raise NotImplementedError()
+    @property
+    def n_dim(self) -> int:
+        """
+        Returns the dimension of the process. If it's univariate it returns a 0, 1 for a vector etc - just like torch.
+        """
 
-    def parameters_from_array(self, array: torch.Tensor, transformed=False):
-        raise NotImplementedError()
+        return len(self.initial_dist().event_shape)
+
+    @property
+    def num_vars(self) -> int:
+        """
+        Returns the number of variables of the stochastic process. E.g. if it's a univariate process it returns 1, and
+        if it's a multivariate process it return the number of elements in the vector or matrix.
+        """
+
+        return self.initial_dist().event_shape.numel()
+
+    def build_initial_density(self, shape: ShapeLike = None) -> Distribution:
+        """
+        Defines and returns the initial density.
+        """
+
+        return self.initial_dist().expand(size_getter(shape))
+
+    def initial_sample(self, shape: ShapeLike = None) -> TimeseriesState:
+        """
+        Samples a state from the initial distribution.
+        """
+
+        return TimeseriesState(0.0, self.build_initial_density(shape).sample())
 
     def log_prob(self, y: torch.Tensor, x: TimeseriesState) -> torch.Tensor:
         """
@@ -46,7 +73,7 @@ class Base(PriorMixin, Module):
         it's an observable process this method corresponds to the observation density, whereas for a hidden process it
         corresponds to the transition density.
 
-        :param x: The current or previous state of the hidden state - depending on whether self is observable or hidden.
+        :param x: The current or previous state of the hidden process, depending on whether self is observable or hidden
         """
 
         raise NotImplementedError()
@@ -85,7 +112,13 @@ class Base(PriorMixin, Module):
         :return: An array of sampled values
         """
 
-        raise NotImplementedError()
+        x_s = self.initial_sample(samples) if x_s is None else x_s
+
+        res = (x_s,)
+        for i in range(1, steps):
+            res += (self.propagate(res[-1]),)
+
+        return torch.stack(tuple(r.state for r in res), dim=0)
 
     def copy(self):
         """
