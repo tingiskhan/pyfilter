07019ab0fe14447426428a20fa8a2db7a967a289
tingis <victor.tingstrom@gmail.com>
2021-04-17 13:16:54 +0200

    Rename

--- test/distribution.py
+++ test/distribution.py
@@ -3,7 +3,7 @@ from pyfilter.distributions import Prior, DistributionWrapper
 from torch.distributions import Exponential, StudentT
 
 
-class MyTestCase(unittest.TestCase):
+class DistributionTests(unittest.TestCase):
     def test_PositivePrior(self):
         prior = Prior(Exponential, rate=0.1)
         dist = DistributionWrapper(StudentT, df=prior)
