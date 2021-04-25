c992866d236ae8c1f759340f46ad08c8821a6519
tingis <victor.tingstrom@gmail.com>
2021-04-25 16:35:27 +0200

    VB now works for unobserved

--- pyfilter/inference/batch/variational/vb.py
+++ pyfilter/inference/batch/variational/vb.py
@@ -5,7 +5,7 @@ from .approximation import StateMeanField, ParameterMeanField
 from .state import VariationalState
 from ..base import OptimizationBasedAlgorithm
 from ...utils import params_from_tensor, eval_prior_log_prob, sample_model
-from ....timeseries import StateSpaceModel, NewState, StochasticDifferentialEquation
+from ....timeseries import StateSpaceModel, NewState
 from ....constants import EPS
 
 
@@ -20,8 +20,7 @@ class VariationalBayes(OptimizationBasedAlgorithm):
         self._n_samples = int(n_samples)
         self._time_inds: torch.Tensor = None
 
-        self._is_sde = isinstance(self._model.hidden, StochasticDifferentialEquation)
-        self._step_size = self._model.hidden.num_steps if self._is_sde else 1
+        self._is_ssm = isinstance(self._model, StateSpaceModel)
 
     def is_converged(self, old_loss, new_loss):
         return ((new_loss - old_loss).abs() < EPS) & (old_loss != new_loss)
@@ -41,7 +40,7 @@ class VariationalBayes(OptimizationBasedAlgorithm):
         param_dist = self.sample_parameter_approximation(state.param_approx)
         entropy = param_dist.entropy()
 
-        if isinstance(self._model, StateSpaceModel):
+        if self._is_ssm:
             state_dist = state.state_approx.dist()
             transformed = state_dist.rsample((self._n_samples,))
 
@@ -52,7 +51,7 @@ class VariationalBayes(OptimizationBasedAlgorithm):
                 x_t.squeeze_(-1)
                 x_tm1.squeeze_(-1)
 
-            y_state = NewState(self._time_inds[1:], values=x_t)
+            y_state = NewState(self._time_inds[1:], values=x_t[:, ::self._model.observe_every_nth_step])
             x_state = NewState(self._time_inds[:-1], values=x_tm1)
 
             x_dist = self._model.hidden.build_density(x_state)
@@ -73,15 +72,18 @@ class VariationalBayes(OptimizationBasedAlgorithm):
         return -(log_likelihood.mean(0) + eval_prior_log_prob(self._model, constrained=False).squeeze().mean() + entropy)
 
     def initialize(self, y, param_approx: ParameterMeanField, state_approx: Optional[StateMeanField] = None):
-        is_ssm = isinstance(self._model, StateSpaceModel)
-
         sample_model(self._model, (self._n_samples, 1))
         param_approx.initialize(y, self._model)
 
         opt_params = tuple(param_approx.parameters())
-        self._time_inds = torch.arange(0, y.shape[0])
 
-        if is_ssm:
+        t_end = y.shape[0]
+        if self._is_ssm:
+            t_end *= self._model.observe_every_nth_step
+
+        self._time_inds = torch.arange(0, t_end)
+
+        if self._is_ssm:
             state_approx.initialize(self._time_inds, self._model)
             opt_params += tuple(state_approx.parameters())
 
