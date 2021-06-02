2dd71ec9068fb23c0de40733cf9fcd7f5e393730
tingis <victor.tingstrom@gmail.com>
2021-06-02 17:33:34 +0200

    Uses monte carlo samples for estimating the noise variance instead, and increase number of samples

--- pyfilter/filters/kalman/unscented/transform.py
+++ pyfilter/filters/kalman/unscented/transform.py
@@ -12,7 +12,7 @@ from ....parameter import ExtendedParameter
 
 
 class UnscentedFilterTransform(Module):
-    MONTE_CARLO_ESTIMATES = 1000
+    MONTE_CARLO_ESTIMATES = 10_000
 
     def __init__(self, model: StateSpaceModel, a=1.0, b=2.0, k=0.0):
         """
@@ -82,11 +82,13 @@ class UnscentedFilterTransform(Module):
         )
 
         cov[..., self._hidden_slc, self._hidden_slc] = construct_diag_from_flat(
-            self._model.hidden.increment_dist().variance, self._model.hidden.n_dim
+            self._model.hidden.increment_dist().sample((self.MONTE_CARLO_ESTIMATES,)).var(0),
+            self._model.hidden.n_dim
         )
 
         cov[..., self._obs_slc, self._obs_slc] = construct_diag_from_flat(
-            self._model.observable.increment_dist().variance, self._model.observable.n_dim
+            self._model.observable.increment_dist().sample((self.MONTE_CARLO_ESTIMATES,)).var(0),
+            self._model.observable.n_dim
         )
 
         dist = MultivariateNormal(loc=mean, covariance_matrix=cov)
