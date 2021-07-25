7b91308abd54eeff5318bb0b161eececb9d93623
Victor <victor.gruselius@gmail.com>
2021-07-25 15:29:19 +0200

    Removes model and allows for initial mean

--- pyfilter/timeseries/models/local_linear_trend.py
+++ pyfilter/timeseries/models/local_linear_trend.py
@@ -52,10 +52,9 @@ def semi_scale(x, alpha, beta, sigma, _):
 def semi_initial_transform(module, base_dist):
     mean_, coef_, scale_, initial_mean = tuple(module.functional_parameters())
 
-    mean_ = concater(initial_mean, mean_)
     scale_ = concater(scale_[..., 0], scale_[..., 1] / (2 * coef_).sqrt())
 
-    return TransformedDistribution(base_dist, AffineTransform(mean_, scale_))
+    return TransformedDistribution(base_dist, AffineTransform(initial_mean, scale_))
 
 
 class SemiLocalLinearTrend(AffineProcess):
@@ -63,7 +62,9 @@ class SemiLocalLinearTrend(AffineProcess):
     Implements a semi local linear trend (see Tensorflow)
     """
 
-    def __init__(self, mean_: ArrayType, coef_: ArrayType, sigma: ArrayType, initial_mean: ArrayType = 0.0, **kwargs):
+    def __init__(
+        self, mean_: ArrayType, coef_: ArrayType, sigma: ArrayType, initial_mean: ArrayType = torch.zeros(2), **kwargs
+    ):
         parameters = (mean_, coef_, sigma, initial_mean)
 
         initial_dist = increment_dist = DistributionWrapper(
@@ -76,43 +77,5 @@ class SemiLocalLinearTrend(AffineProcess):
             initial_dist=initial_dist,
             increment_dist=increment_dist,
             initial_transform=semi_initial_transform,
-            **kwargs
-        )
-
-
-def trending_reversion_mean(x, mean_, coef_, sigma, _):
-    slope = x.values[..., 1]
-
-    new_level = mean_ + coef_ * (x.values[..., 0] - mean_) + slope
-    return concater(new_level, slope)
-
-
-def trending_initial_transform(module, base_dist):
-    mean_, coef_, scale_, initial_mean = tuple(module.functional_parameters())
-
-    mean_ = concater(mean_, initial_mean)
-    scale_ = concater(scale_[..., 0] / (1 - coef_ ** 2).sqrt(), scale_[..., 1])
-
-    return TransformedDistribution(base_dist, AffineTransform(mean_, scale_))
-
-
-class TrendingMeanReversion(AffineProcess):
-    """
-    Implements a mean reversion process exhibiting trending behaviour.
-    """
-
-    def __init__(self, mean_: ArrayType, coef_: ArrayType, sigma: ArrayType, initial_mean: ArrayType = 0.0, **kwargs):
-        parameters = (mean_, coef_, sigma, initial_mean)
-
-        initial_dist = increment_dist = DistributionWrapper(
-            lambda **u: Independent(Normal(**u), 1), loc=torch.zeros(2), scale=torch.ones(2)
-        )
-
-        super(TrendingMeanReversion, self).__init__(
-            (trending_reversion_mean, semi_scale),
-            parameters,
-            initial_dist=initial_dist,
-            increment_dist=increment_dist,
-            initial_transform=trending_initial_transform,
             **kwargs
         )
