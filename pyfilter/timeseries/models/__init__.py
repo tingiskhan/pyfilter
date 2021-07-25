bf475d546de6783e37efb07f40530a25ab1731ea
tingiskhan <victor.tingstrom@gmail.com>
2021-07-25 16:14:29 +0200

    More llt (#102)
    * Removes model and allows for initial mean
    
    * __init__ fixes
    
    Co-authored-by: Victor <victor.gruselius@gmail.com>

--- pyfilter/timeseries/models/__init__.py
+++ pyfilter/timeseries/models/__init__.py
@@ -2,7 +2,7 @@ from .ou import OrnsteinUhlenbeck
 from .verhulst import Verhulst
 from .ar import AR
 from .epidemiological import OneFactorSIR
-from .local_linear_trend import LocalLinearTrend, SemiLocalLinearTrend, TrendingMeanReversion
+from .local_linear_trend import LocalLinearTrend, SemiLocalLinearTrend
 from .llt_sv import LocalLinearTrendWithStochasticVolatility
 
 __all__ = [
@@ -12,6 +12,5 @@ __all__ = [
     "OneFactorSIR",
     "LocalLinearTrend",
     "SemiLocalLinearTrend",
-    "TrendingMeanReversion",
     "LocalLinearTrendWithStochasticVolatility",
 ]
