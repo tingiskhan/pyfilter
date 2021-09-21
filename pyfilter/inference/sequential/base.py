9dd6070f04afcb0c883239163b1ce536efe1209c
tingis <victor.tingstrom@gmail.com>
2021-09-21 23:00:15 +0200

    Docs

--- pyfilter/inference/sequential/base.py
+++ pyfilter/inference/sequential/base.py
@@ -1,21 +1,28 @@
 from abc import ABC
 import torch
+from typing import Dict, Any
 from .state import SequentialAlgorithmState
 from ..logging import TQDMWrapper
 from ..base import BaseFilterAlgorithm
 from ..utils import sample_model
-from ...utils import normalize
-from ...filters import ParticleFilter, FilterResult
+from ...filters import ParticleFilter
 
 
 class SequentialFilteringAlgorithm(BaseFilterAlgorithm, ABC):
     """
-    Base class for sequential algorithms using filters in order to approximate the log likelihood.
+    Abstract base class for sequential algorithms using filters in order to approximate the log likelihood.
     """
 
     def update(self, y: torch.Tensor, state: SequentialAlgorithmState) -> SequentialAlgorithmState:
         """
-        Performs an update using a single observation `y`.
+        Updates the algorithm and filter state given the latest observation ``y``.
+
+        Args:
+            y: The latest observation.
+            state: The previous state of the algorithm.
+
+        Returns:
+            The updated state of the algorithm.
         """
 
         raise NotImplementedError()
@@ -40,10 +47,18 @@ class SequentialFilteringAlgorithm(BaseFilterAlgorithm, ABC):
 
 class SequentialParticleAlgorithm(SequentialFilteringAlgorithm, ABC):
     """
-    Base class for sequential algorithms using particles to approximate the distribution of the parameters.
+    Abstract base class for Bayesian sequential algorithms that use a particle approximation of the parameter posterior.
     """
 
     def __init__(self, filter_, particles: int):
+        """
+        Initializes the ``SequentialParticleAlgorithm`` class.
+
+        Args:
+            filter_: See base.
+            particles: The number of particles to use for approximating the parameter posteriors.
+        """
+
         super().__init__(filter_)
 
         self.register_buffer("_particles", torch.tensor(particles, dtype=torch.int))
@@ -51,14 +66,22 @@ class SequentialParticleAlgorithm(SequentialFilteringAlgorithm, ABC):
 
     @property
     def particles(self) -> torch.Size:
+        """
+        Returns the number of particles the algorithm uses.
+        """
+
         return torch.Size([self._particles])
 
-    def sample_params(self):
+    def _sample_params(self):
+        """
+        Samples the parameters of the model in place.
+        """
+
         shape = torch.Size((*self.particles, 1)) if isinstance(self.filter, ParticleFilter) else self.particles
         sample_model(self.filter.ssm, shape)
 
     def initialize(self) -> SequentialAlgorithmState:
-        self.sample_params()
+        self._sample_params()
 
         init_state = self.filter.initialize()
         init_weights = torch.zeros(self.particles, device=init_state.get_loglikelihood().device)
@@ -81,12 +104,24 @@ class SequentialParticleAlgorithm(SequentialFilteringAlgorithm, ABC):
 
 
 class CombinedSequentialParticleAlgorithm(SequentialParticleAlgorithm, ABC):
-    def __init__(self, filter_, particles, switch: int, first_kw, second_kw):
-        """
-        Algorithm combining two instances of `SequentialParticleAlgorithm`. One such example is the `NESSMC2`, where we
-        utilize the `SMC2` for an arbitrary chunk of the data first, and then switch to the `NESS` algorithm.
+    """
+    Algorithm combining two instances of ``SequentialParticleAlgorithm``, where we let one of them target a
+    chronological subset of the data, and the other the remaining points.
 
-        :param switch: After how many observations to perform switch
+    One such example is the ``NESSMC2``, where we first utilize the costly but exact ``SMC2`` algorithm, and then switch
+    to the ``NESS`` which is a pure online algorithm, but with slower convergence than ``SMC2``.
+    """
+
+    def __init__(self, filter_, particles, switch: int, first_kw: Dict[str, Any], second_kw: Dict[str, Any]):
+        """
+        Initializes the ``CombinedSequentialParticleAlgorithm`` class.
+
+        Args:
+            filter_: See base.
+            particles: See base.
+            switch: The number of observations to have parsed before switching algorithms.
+            first_kw: Kwargs sent to ``.make_first(...)``.
+            second_kw: Kwargs sent to ``.make_second(...)``.
         """
 
         super().__init__(filter_, particles)
@@ -97,9 +132,25 @@ class CombinedSequentialParticleAlgorithm(SequentialParticleAlgorithm, ABC):
         self._num_iters = 0
 
     def make_first(self, filter_, particles, **kwargs) -> SequentialParticleAlgorithm:
+        """
+        Creates the algorithm to be used for the first part of the data.
+
+        Args:
+            filter_: See ``__init__``.
+            particles: See ``__init__``.
+            kwargs: Corresponds to ``first_kw`` of ``__init__``.
+
+        Returns:
+            Instance of algorithm to be used for first part of the data.
+        """
+
         raise NotImplementedError()
 
     def make_second(self, filter_, particles, **kwargs) -> SequentialParticleAlgorithm:
+        """
+        See ``.make_first(...)`` but replace `first` with `second`.
+        """
+
         raise NotImplementedError()
 
     def do_on_switch(
