1a703175c1dc1d4a2065e1a2a719eadfc7286dc4
Victor <victor.gruselius@gmail.com>
2021-10-02 15:42:25 +0200

    First rework of tests and adds ``pytest`` requirement

---
+++ tests/distributions.py
@@ -0,0 +1,93 @@
+import pytest
+import torch
+from pyfilter.distributions import Prior, DistributionWrapper, JointDistribution
+from torch.distributions import (
+    Exponential,
+    StudentT,
+    Independent,
+    AffineTransform,
+    TransformedDistribution,
+    ExpTransform,
+)
+
+
+@pytest.fixture
+def joint_distribution():
+    dist_1 = Exponential(rate=1.0)
+    dist_2 = Independent(StudentT(df=torch.ones(2)), 1)
+
+    return JointDistribution(dist_1, dist_2)
+
+
+class TestDistributions(object):
+    def test_PositivePrior(self):
+        prior = Prior(Exponential, rate=0.1)
+        wrapper = DistributionWrapper(StudentT, df=prior)
+
+        dist = wrapper()
+
+        assert isinstance(dist, StudentT) and (dist.df > 0).all()
+
+
+class TestJointDistribution(object):
+    def test_JointDistributionMasksAndShape(self, joint_distribution):
+        assert joint_distribution.indices[0] == 0
+        assert joint_distribution.indices[1] == slice(1, 3)
+
+        assert joint_distribution.event_shape == torch.Size([3])
+
+    def test_JointDistributionSamples(self, joint_distribution):
+        samples = joint_distribution.sample()
+
+        assert samples.shape == torch.Size([3])
+
+        more_sample_shape = torch.Size([1000, 300])
+        more_samples = joint_distribution.sample(more_sample_shape)
+
+        assert more_samples.shape == torch.Size([*more_sample_shape, 3])
+
+    def test_JointDistributionLogProb(self, joint_distribution):
+        shape = torch.Size([1000, 300])
+
+        samples = joint_distribution.sample(shape)
+        log_prob = joint_distribution.log_prob(samples)
+
+        assert log_prob.shape == shape
+
+    def test_JointDistributionEntropy(self, joint_distribution):
+        expected = joint_distribution.distributions[0].entropy() + joint_distribution.distributions[1].entropy()
+        assert joint_distribution.entropy() == expected
+
+    def test_CheckGradient(self, joint_distribution):
+        samples = joint_distribution.sample()
+
+        joint_distribution.distributions[0].rate.requires_grad_(True)
+        log_prob = joint_distribution.log_prob(samples)
+
+        assert log_prob.requires_grad
+
+        log_prob.backward()
+        assert joint_distribution.distributions[0].rate.grad is not None
+
+    def test_CheckExpand(self, joint_distribution):
+        new_shape = torch.Size([1000, 10])
+        expanded = joint_distribution.expand(new_shape)
+
+        assert expanded.batch_shape == new_shape
+
+    def test_TransformJointDistribution(self, joint_distribution):
+        transformed = TransformedDistribution(joint_distribution, AffineTransform(0.0, 0.0))
+        samples = transformed.sample()
+
+        assert (samples == 0.0).all()
+
+        mean = 1.0
+        transformed = TransformedDistribution(joint_distribution, AffineTransform(mean, 1.0))
+        samples = transformed.sample()
+
+        assert transformed.log_prob(samples) == joint_distribution.log_prob(samples - mean)
+
+        exp_transform = TransformedDistribution(joint_distribution, ExpTransform())
+        samples = exp_transform.sample((1000,))
+
+        assert (samples >= 0.0).all()
