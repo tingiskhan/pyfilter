741104e79f9c907bba909e5c41b7d16b2f515c50
Victor <victor.gruselius@gmail.com>
2021-10-11 16:43:47 +0200

    Add max value instead

--- pyfilter/constants.py
+++ pyfilter/constants.py
@@ -3,5 +3,9 @@ from math import sqrt
 
 
 INFTY = float("inf")
-EPS = sqrt(torch.finfo(torch.get_default_dtype()).eps)
-EPS2 = EPS ** 2.0
+_info = torch.finfo(torch.get_default_dtype())
+
+EPS = sqrt(_info.eps)
+EPS2 = _info.eps
+
+MAX = _info.max
