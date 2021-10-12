ef2b4153f7330fcd983e5089c7c5f57af4614ed9
Victor <victor.gruselius@gmail.com>
2021-10-12 14:58:28 +0200

    Adds PMMH tests

--- tests/inference.py
+++ tests/inference.py
@@ -6,11 +6,12 @@ from tests.filters import construct_filters
 from pyfilter.inference.sequential import NESS, SMC2, SMC2FW, NESSMC2
 from scipy.stats import gaussian_kde
 from pyfilter.inference.batch.variational import VariationalBayes, approximation as apx
+from pyfilter.inference.batch.mcmc import PMMH, proposals as props
 
 
 @pytest.fixture
 def models():
-    ou = m.OrnsteinUhlenbeck(0.01, 0.0, 0.05, dt=1.0)
+    ou = m.OrnsteinUhlenbeck(0.025, 0.0, 0.05, dt=1.0)
     obs_1d = LinearGaussianObservations(ou, 1.0, 0.05)
 
     ou_priors = (
@@ -18,20 +19,11 @@ def models():
         Prior(Normal, loc=0.0, scale=1.0),
         Prior(LogNormal, loc=0.0, scale=1.0)
     )
-    prob_ou = m.OrnsteinUhlenbeck(*ou_priors, dt=ou._dt.clone())
+    prob_ou = m.OrnsteinUhlenbeck(*ou_priors, dt=ou._dt.clone().detach())
     prob_obs_1d = LinearGaussianObservations(prob_ou, obs_1d.observable.parameter_0, obs_1d.observable.parameter_1)
 
     return (
         [prob_obs_1d, obs_1d],
-    )
-
-
-def sequential_algorithms(filter_, **kwargs):
-    return (
-        NESS(filter_, **kwargs),
-        SMC2(filter_, **kwargs),
-        SMC2FW(filter_, **kwargs),
-        NESSMC2(filter_, **kwargs),
     )
 
 
@@ -47,12 +39,21 @@ class TestsSequentialAlgorithm(object):
     PARTICLES = 1000
     SERIES_LENGTH = 1000
 
+    @staticmethod
+    def sequential_algorithms(filter_, **kwargs):
+        return (
+            NESS(filter_, **kwargs),
+            SMC2(filter_, **kwargs),
+            SMC2FW(filter_, **kwargs),
+            NESSMC2(filter_, **kwargs),
+        )
+
     def test_sequential_algorithms(self, models):
         for prob_model, model in models:
             x, y = model.sample_path(self.SERIES_LENGTH)
 
             for f in construct_filters(prob_model):
-                for algorithm in sequential_algorithms(f.copy(), particles=self.PARTICLES):
+                for algorithm in self.sequential_algorithms(f.copy(), particles=self.PARTICLES):
                     result = algorithm.fit(y)
 
                     w = result.normalized_weights()
@@ -61,7 +62,7 @@ class TestsSequentialAlgorithm(object):
                         prior = get_prior(name, algorithm)
                         true_parameter = get_true_parameter(name, model)
 
-                        kde = gaussian_kde(prior.get_unconstrained(parameter).squeeze().numpy(), weights=w.numpy())
+                        kde = gaussian_kde(prior.get_unconstrained(parameter).squeeze(dim=1).numpy(), weights=w.numpy())
 
                         inverse_true_value = prior.bijection.inv(true_parameter)
 
@@ -72,9 +73,12 @@ class TestsSequentialAlgorithm(object):
 
 
 class TestBatchAlgorithms(object):
+    SERIES_LENGTH = 500
+    BURN_IN = 500
+
     def test_variational_bayes(self, models):
         for prob_model, model in models:
-            x, y = model.sample_path(TestsSequentialAlgorithm.SERIES_LENGTH)
+            x, y = model.sample_path(self.SERIES_LENGTH)
 
             algorithm = VariationalBayes(
                 prob_model,
@@ -95,4 +99,35 @@ class TestBatchAlgorithms(object):
 
                 assert posterior.log_prob(true_parameter) > prior.log_prob(true_parameter)
 
-# TODO: Fix PMMH tests
+    @staticmethod
+    def pmmh_proposals(filter_, **kwargs):
+        return (
+            PMMH(filter_, proposal=props.RandomWalk(scale=0.05), **kwargs),
+            PMMH(filter_, proposal=props.GradientBasedProposal(scale=0.05), **kwargs),
+            PMMH(filter_, proposal=props.GradientBasedProposal(scale=0.025), **kwargs),
+        )
+
+    def test_pmmh(self, models):
+        for prob_model, model in models:
+            x, y = model.sample_path(self.SERIES_LENGTH)
+
+            for f in construct_filters(prob_model):
+                for algorithm in self.pmmh_proposals(f.copy(), samples=2 * self.BURN_IN, num_chains=4):
+                    result = algorithm.fit(y)
+
+                    numel = 0
+                    for name, _ in algorithm.filter.ssm.named_parameters():
+                        prior = get_prior(name, algorithm)
+                        true_parameter = get_true_parameter(name, model)
+
+                        slc, numel = prior.get_slice_for_parameter(numel)
+                        parameter = result.samples.values()[self.BURN_IN:, ..., slc].view(-1, numel)
+
+                        kde = gaussian_kde(prior.get_unconstrained(parameter).squeeze(dim=1).numpy())
+
+                        inverse_true_value = prior.bijection.inv(true_parameter)
+
+                        posterior_log_prob = kde.logpdf(inverse_true_value)
+                        prior_log_prob = prior.unconstrained_prior.log_prob(inverse_true_value).numpy()
+
+                        assert posterior_log_prob > prior_log_prob
