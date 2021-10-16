9b928b479747f894a607698dd77506a9ea9de7d4
Victor <victor.gruselius@gmail.com>
2021-10-16 20:42:51 +0200

    Adds method for updating parameters from chain

--- pyfilter/inference/batch/mcmc/state.py
+++ pyfilter/inference/batch/mcmc/state.py
@@ -2,6 +2,7 @@ import torch
 from ...state import FilterAlgorithmState
 from ....filters import FilterResult
 from ....container import TensorTuple
+from ....timeseries import StateSpaceModel
 
 
 class PMMHResult(FilterAlgorithmState):
@@ -34,3 +35,18 @@ class PMMHResult(FilterAlgorithmState):
         """
 
         self.samples.append(sample)
+
+    def update_parameters_from_chain(self, model: StateSpaceModel, burn_in: int):
+        """
+        Sets the parameters of ``model`` from chain indexed from ``burn_in`` and forward.
+
+        Args:
+            model: The model to set the parameters for.
+            burn_in: The number of samples from the chain to discard.
+        """
+
+        samples = self.samples.values()[burn_in:]
+        samples = samples.flatten(end_dim=-2)
+
+        model.sample_params(torch.Size([samples.shape[0], 1]))
+        model.update_parameters_from_tensor(samples, constrained=False)
