a0ab1cf7f57c8bfe51770180906b6fb0549f0504
tingiskhan <victor.tingstrom@gmail.com>
2021-10-20 19:56:22 +0200

    Sin arcsinh (#111)
    * Adds the SinhArchsinh transform and some tests
    
    * Updates version

--- pyfilter/distributions/__init__.py
+++ pyfilter/distributions/__init__.py
@@ -1,6 +1,7 @@
 from .wrapper import DistributionWrapper
 from .prior import Prior
 from .joint import JointDistribution
+from .sinh_arcsinh import SinhArcsinhTransform
 
 
-__all__ = ["DistributionWrapper", "Prior", "JointDistribution"]
+__all__ = ["DistributionWrapper", "Prior", "JointDistribution", "SinhArcsinhTransform"]
