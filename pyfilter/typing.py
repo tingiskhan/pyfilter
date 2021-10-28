72a7d4aea083db2ea6397ef395a993c3a99c991b
tingis <victor.tingstrom@gmail.com>
2021-10-28 10:14:44 +0200

    Use numbers.Number instead

--- pyfilter/typing.py
+++ pyfilter/typing.py
@@ -1,8 +1,9 @@
 import numpy as np
 from typing import Union, Tuple
 import torch
+from numbers import Number
 from .distributions import Prior
 
 
-ArrayType = Union[float, int, np.ndarray, Prior, torch.Tensor]
+ArrayType = Union[Number, np.ndarray, Prior, torch.Tensor]
 ShapeLike = Union[int, Tuple[int, ...], torch.Size]
