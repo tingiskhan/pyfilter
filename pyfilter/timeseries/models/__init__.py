9650a368f7566be86c6eaf18e0e849a264b97902
tingiskhan <victor.tingstrom@gmail.com>
2021-12-22 17:52:04 +0100

    Smooth trend (#116)
    * Adds `SmoothTrendModel`
    
    * Adds `SmoothLinearTrend` to tests

--- pyfilter/timeseries/models/__init__.py
+++ pyfilter/timeseries/models/__init__.py
@@ -2,7 +2,7 @@ from .ou import OrnsteinUhlenbeck
 from .verhulst import Verhulst
 from .ar import AR
 from .epidemiological import OneFactorSIR
-from .local_linear_trend import LocalLinearTrend, SemiLocalLinearTrend
+from .local_linear_trend import LocalLinearTrend, SemiLocalLinearTrend, SmoothLinearTrend
 from .random_walk import RandomWalk
 from .ucsv import UCSV
 
@@ -15,4 +15,5 @@ __all__ = [
     "SemiLocalLinearTrend",
     "RandomWalk",
     "UCSV",
+    "SmoothLinearTrend",
 ]
