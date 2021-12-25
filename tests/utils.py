17fae19fc9b04a0d9ccb3d003cab49f10f938680
tingis <victor.tingstrom@gmail.com>
2021-12-25 11:23:30 +0100

    Adds convenience function for broadcasting

---
+++ tests/utils.py
@@ -0,0 +1,35 @@
+import pytest
+import torch
+from pyfilter.utils import broadcast_all
+from torch.distributions import Normal, utils
+from pyfilter.distributions import Prior
+
+
+@pytest.fixture()
+def tensors():
+    return [torch.empty(100).normal_(), torch.tensor(0.0)]
+
+
+@pytest.fixture()
+def mixed(tensors):
+    return tensors + [Prior(Normal, loc=0.0, scale=1.0), torch.tensor(2.0)]
+
+
+class TestUtils(object):
+    def test_broadcast_all_tensors(self, tensors):
+        torch_broadcast = utils.broadcast_all(*tensors)
+        broadcast = broadcast_all(*tensors)
+
+        for correct, value in zip(torch_broadcast, broadcast):
+            assert (correct == value).all()
+
+    def test_broadcast_all_mixed(self, mixed):
+        torch_broadcast = utils.broadcast_all(*(v for v in mixed if not isinstance(v, Prior)))
+        broadcast = broadcast_all(*mixed)
+
+        assert len(torch_broadcast) + 1 == len(broadcast)
+
+        for correct, value in zip(torch_broadcast, (v for v in broadcast if isinstance(v, torch.Tensor))):
+            assert (correct == value).all()
+
+        assert mixed[-2] in broadcast
