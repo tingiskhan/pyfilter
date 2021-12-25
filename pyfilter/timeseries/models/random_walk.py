5152a2ede7e82c5bfb7e4d01cb37c02c195c3442
tingis <victor.tingstrom@gmail.com>
2021-12-25 21:06:54 +0100

    LLT and RandomWalk now utilize LinearModel

--- pyfilter/timeseries/models/random_walk.py
+++ pyfilter/timeseries/models/random_walk.py
@@ -1,27 +1,12 @@
-from torch.distributions import Normal, AffineTransform, TransformedDistribution
+from torch.distributions import Normal
 import torch
-from numbers import Number
-from ..affine import AffineProcess
+from ..linear_model import LinearModel
 from ...distributions import DistributionWrapper
 from ...typing import ArrayType
+from ...utils import broadcast_all
 
 
-def _f(x, s, _):
-    return x.values
-
-
-def _g(x, s, _):
-    return s
-
-
-def init_trans(model: "RandomWalk", base_dist):
-    sigma, mean = tuple(model.functional_parameters())
-    initial_mean = mean if mean is not None else base_dist.mean
-
-    return TransformedDistribution(base_dist, AffineTransform(initial_mean, sigma))
-
-
-class RandomWalk(AffineProcess):
+class RandomWalk(LinearModel):
     """
     Defines a Gaussian random walk process, i.e. in which the dynamics are given by
         .. math::
@@ -31,26 +16,23 @@ class RandomWalk(AffineProcess):
     where :math:`x_0` is the initial mean, defaulting to zero.
     """
 
-    def __init__(self, std: ArrayType, initial_mean: ArrayType = None, **kwargs):
+    def __init__(self, std: ArrayType, initial_mean: ArrayType = 0.0, initial_scale: ArrayType = 1.0, **kwargs):
         """
         Initializes the ``RandomWalk`` model.
 
         Args:
             std: Corresponds to :math:`\\sigma` in class doc.
-            initial_mean: Optional parameter specifying the mean of the initial values. Defaults to zero if ``None``.
+            initial_mean: Optional parameter specifying the mean of the initial distribution. Defaults to 0.
+            initial_scale: Optional parameter specifying the scale of the initial distribution. Defaults to 1.
             kwargs: See base.
         """
 
-        if isinstance(std, Number):
-            normal = DistributionWrapper(Normal, loc=0.0, scale=1.0)
-        else:
-            shape = std.shape
+        std, initial_mean, initial_scale = broadcast_all(std, initial_mean, initial_scale)
 
-            if len(shape) == 0:
-                normal = DistributionWrapper(Normal, loc=0.0, scale=1.0)
-            else:
-                normal = DistributionWrapper(
-                    Normal, loc=torch.zeros(shape), scale=torch.ones(shape), reinterpreted_batch_ndims=1
-                )
+        # TODO: Check all instead, as they might share std?
+        reinterpreted_batch_ndims = None if len(std.shape) == 0 else 1
+        dist = DistributionWrapper(
+            Normal, loc=initial_mean, scale=initial_scale, reinterpreted_batch_ndims=reinterpreted_batch_ndims
+        )
 
-        super().__init__((_f, _g), (std, initial_mean), normal, normal, initial_transform=init_trans, **kwargs)
+        super().__init__(1.0, std, dist, **kwargs)
