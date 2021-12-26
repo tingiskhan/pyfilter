df566abc6caead24d0611fc6295ae211a7bf64c8
tingis <victor.tingstrom@gmail.com>
2021-12-26 17:47:46 +0100

    Adds an ``AffineChainedStochasticProcess``

--- pyfilter/timeseries/chained.py
+++ pyfilter/timeseries/chained.py
@@ -1,4 +1,5 @@
-from .joint import JointStochasticProcess
+import torch
+from .joint import JointStochasticProcess, AffineJointStochasticProcess
 from ..distributions import JointDistribution
 
 
@@ -15,3 +16,23 @@ class ChainedStochasticProcess(JointStochasticProcess):
             *(self._modules[name].build_density(x[: i + 1]) for i, name in enumerate(self._proc_names)),
             indices=self.indices
         )
+
+
+class AffineChainedStochasticProcess(AffineJointStochasticProcess):
+    """
+    Similar to ``ChainedStochasticProcess``, but all sub-processes must be of affine type - similar to
+    ``AffineJointStochasticProcess``.
+    """
+
+    def mean_scale(self, x, parameters=None):
+        mean = tuple()
+        scale = tuple()
+
+        for i, (proc_name, p) in enumerate(zip(self._proc_names, parameters or len(self._proc_names) * [None])):
+            proc = self._modules[proc_name]
+            m, s = torch.broadcast_tensors(*proc.mean_scale(x[:i + 1], p))
+
+            mean += (m.unsqueeze(-1) if proc.n_dim == 0 else m,)
+            scale += (s.unsqueeze(-1) if proc.n_dim == 0 else s,)
+
+        return torch.cat(mean, dim=-1), torch.cat(scale, dim=-1)
