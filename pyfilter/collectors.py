3bbdd54d97c35657f2dfda0bb3f96b6e6c48b3f1
tingis <victor.tingstrom@gmail.com>
2022-03-05 11:14:29 +0100

    Adds module for collecting stuff

---
+++ pyfilter/collectors.py
@@ -0,0 +1,89 @@
+from typing import Callable, TypeVar, Tuple, Generic
+
+from torch.distributions import TransformedDistribution
+
+from .filters.particle.state import ParticleFilterState
+from .inference.sequential.state import SequentialAlgorithmState
+from .state import BaseState
+import torch.nn
+
+from .timeseries import StateSpaceModel
+
+T = TypeVar("T", bound=BaseState)
+
+
+__all__ = [
+    "Collector",
+    "MeanCollector",
+    "Standardizer"
+]
+
+
+class Collector(Generic[T]):
+    """
+    Defines a collector that calculates some weighted calculations and saves to ``.tensor_tuples``.
+    """
+
+    def __init__(self, name: str, f: Callable[[torch.nn.Module, torch.Tensor, T, T], None]):
+        """
+        Initializes the ``Collector`` class.
+
+        Args:
+            name: The name to assign to the ``tensor_tuple``.
+            f: The function to use when calculating statics.
+        """
+
+        self._name = name
+        self._f = f
+
+    def __call__(self, module: torch.nn.Module, inp: Tuple[torch.Tensor, T], out: T):
+        if self._name not in out.tensor_tuples:
+            out.tensor_tuples[self._name] = tuple()
+
+        out.tensor_tuples[self._name] += (self._f(module, *inp, out),)
+
+
+class MeanCollector(Collector[SequentialAlgorithmState]):
+    """
+    Defines a collector that calculates averaged mean, used in sequential algorithms.
+    """
+
+    @staticmethod
+    def _mean(algorithm, y, _: SequentialAlgorithmState, new_state: SequentialAlgorithmState):
+        latest_means = new_state.filter_state.latest_state.get_mean()
+
+        return new_state.normalized_weights() @ latest_means
+
+    def __init__(self):
+        super().__init__(name="filter_means", f=self._mean)
+
+
+class Standardizer(Collector[SequentialAlgorithmState]):
+    """
+    Defines a collector that calculates the residuals.
+    """
+
+    @staticmethod
+    def _standardize(ssm: StateSpaceModel, y, x):
+        y_std = y
+        dist = ssm.observable.build_density(x)
+
+        if not isinstance(dist, TransformedDistribution):
+            raise NotImplementedError(f"Can't standardize for '{dist.__class__.__name__}'")
+
+        for t in reversed(dist.transforms):
+            y_std = t.inv(y_std)
+
+        return y_std
+
+    def _fun(self, algorithm, y, _: SequentialAlgorithmState, new_state: SequentialAlgorithmState):
+        filter_state = new_state.filter_state.latest_state
+
+        residuals = self._standardize(algorithm.filter.ssm, y, filter_state.get_timeseries_state())
+        if isinstance(filter_state, ParticleFilterState):
+            residuals = (filter_state.normalized_weights() * residuals).sum(dim=-1)
+
+        return new_state.normalized_weights() @ residuals
+
+    def __init__(self):
+        super().__init__(name="standardized", f=self._fun)
