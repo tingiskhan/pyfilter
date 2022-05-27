7d09b3b0d504cce34f806994f2e1b850a29d7b4b
tingis <victor.tingstrom@gmail.com>
2022-05-27 20:30:04 +0200

    Removal of files

--- pyfilter/timeseries/typing.py
+++
@@ -1,8 +0,0 @@
-from torch.distributions import Distribution
-from typing import Tuple, Callable
-import torch
-from .state import NewState
-
-
-MeanOrScaleFun = Callable[[NewState, Tuple[torch.Tensor, ...]], torch.Tensor]
-DiffusionFunction = Callable[[NewState, float, Tuple[torch.Tensor, ...]], Distribution]
--- pyfilter/typing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from typing import Union, Tuple
-import torch
-from numbers import Number
-from .distributions import Prior
-
-
-ArrayType = Union[Number, np.ndarray, Prior, torch.Tensor]
-ShapeLike = Union[int, Tuple[int, ...], torch.Size]
