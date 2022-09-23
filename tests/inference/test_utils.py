d7c00d0d1f8b373c8611e92ce0da05f929bafd6d
victor <baj>
2022-09-23 19:23:09 +0200

    Adds test for QMC MVN

---
+++ tests/inference/test_utils.py
@@ -0,0 +1,16 @@
+import pytest
+import torch
+
+from pyfilter.inference import utils, qmc
+
+BATCH_SHAPES = [torch.Size([]), torch.Size([512, 1]), torch.Size([50, 2, 3])]
+
+
+class TestUtils(object):
+    @pytest.mark.parametrize("shape", BATCH_SHAPES)
+    def test_quasi_mv(self, shape):
+        qmc.QuasiRegistry.clear_registry()
+        mv = utils.QuasiMultivariateNormal(torch.zeros(3), torch.eye(3))
+        samples = mv.sample(shape)
+
+        assert (samples.shape == shape + mv.event_shape)
