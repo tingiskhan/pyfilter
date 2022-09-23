c8316eb7e79702c21e9be9b5179f6700aea66267
victor <baj>
2022-09-23 19:22:56 +0200

    Implements a custom quasi multivariate normal class

--- pyfilter/inference/utils.py
+++ pyfilter/inference/utils.py
@@ -1,17 +1,46 @@
-from torch.distributions import MultivariateNormal
+from dataclasses import dataclass
+
+from pyro.distributions import MultivariateNormal, Normal
 import torch
 from torch.linalg import cholesky_ex
 
+from .qmc import QuasiRegistry
 
-def construct_mvn(x: torch.Tensor, w: torch.Tensor, scale=1.0) -> MultivariateNormal:
+
+@dataclass
+class MeanChol(object):
+    mean: torch.Tensor
+    chol: torch.Tensor
+
+
+class QuasiMultivariateNormal(MultivariateNormal):
+    r"""
+    Implements a multivariate random normal class which enables sampling by inversion.
     """
-    Constructs a multivariate normal distribution from weighted samples.
 
-    Args:
-        x: samples from which to create the the multivariate normal distribution, should be of size
-            ``(batch size, dimension of space)``.
-        w: weights associated to each point of ``x``, should therefore be of size ``batch size``.
-        scale: applies a scaling to the Cholesky factorized covariance matrix of the distribution.
+    has_rsample = False
+
+    def sample(self, sample_shape=torch.Size()):
+        shape = self._extended_shape(sample_shape)
+
+        # TODO: This should be in __init__...
+        QuasiRegistry.add_engine(self.event_shape.numel())
+        probs = QuasiRegistry.sample(self.event_shape.numel(), sample_shape).to(self.loc.device)
+
+        probs.resize_(shape)
+
+        loc = torch.zeros(shape, device=self.loc.device)
+        scale = torch.ones(shape, device=self.loc.device)
+        normal = Normal(loc, scale)
+
+        eps = normal.icdf(probs)
+
+        return self.loc + self._unbroadcasted_scale_tril.matmul(eps.unsqueeze(-1)).squeeze(-1)
+
+
+def calc_mean_chol(x: torch.Tensor, w: torch.Tensor) -> MeanChol:
+    r"""
+    Calculates the mean and covariance for a multivariate normal density.
     """
 
     mean = w @ x
@@ -23,4 +52,25 @@ def construct_mvn(x: torch.Tensor, w: torch.Tensor, scale=1.0) -> MultivariateNo
     if (info > 0).any():
         cholesky = cov.diag().sqrt().diag()
 
-    return MultivariateNormal(mean, scale_tril=scale * cholesky)
+    return MeanChol(mean, cholesky)
+
+
+def construct_mvn(x: torch.Tensor, w: torch.Tensor, scale=1.0, quasi: bool = False) -> MultivariateNormal:
+    """
+    Constructs a multivariate normal distribution from weighted samples.
+
+    Args:
+        x: samples from which to create the the multivariate normal distribution, should be of size
+            ``(batch size, dimension of space)``.
+        w: weights associated to each point of ``x``, should therefore be of size ``batch size``.
+        scale: applies a scaling to the Cholesky factorized covariance matrix of the distribution.
+        quasi: whether to use quasi random sampling.
+    """
+
+    mean_chol = calc_mean_chol(x, w)
+    scale_tril = scale * mean_chol.chol
+
+    if not quasi:
+        return MultivariateNormal(mean_chol.mean, scale_tril=scale_tril)
+
+    return QuasiMultivariateNormal(mean_chol.mean, scale_tril=scale_tril)
