e9e078365c52082d62a8c083500f6cbe69ba04e1
victor <baj>
2022-09-23 19:27:02 +0200

    Adds parameter specifying wheter quasi or not

--- pyfilter/inference/batch/mcmc/proposals/symmetric_mh.py
+++ pyfilter/inference/batch/mcmc/proposals/symmetric_mh.py
@@ -3,6 +3,7 @@ from .base import BaseProposal
 from ....utils import construct_mvn
 
 
+# TODO: Should perhaps create a quasi-specific class for this...?
 class SymmetricMH(BaseProposal):
     """
     Builds a symmetric proposal as defined in the original `SMC2` paper.
@@ -12,7 +13,7 @@ class SymmetricMH(BaseProposal):
         values = context.stack_parameters(constrained=False)
         weights = state.normalized_weights()
 
-        return construct_mvn(values, weights, scale=1.1)  # Same scale in in num_particles
+        return construct_mvn(values, weights, scale=1.1, quasi=context.is_quasi)  # Same scale in in num_particles
 
     def exchange(self, latest, candidate, mask: torch.Tensor) -> None:
         return
