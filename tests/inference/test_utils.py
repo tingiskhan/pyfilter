e57534afec6d112faa1c5195d39c90f39ae1889b
victor <baj>
2022-09-24 13:42:24 +0200

    Clears registry as fixture

--- tests/inference/test_utils.py
+++ tests/inference/test_utils.py
@@ -6,10 +6,14 @@ from pyfilter.inference import utils, qmc
 BATCH_SHAPES = [torch.Size([]), torch.Size([512, 1]), torch.Size([50, 2, 3])]
 
 
+@pytest.fixture()
+def clear_registry():
+    qmc.QuasiRegistry.clear_registry()
+
+
 class TestUtils(object):
     @pytest.mark.parametrize("shape", BATCH_SHAPES)
-    def test_quasi_mv(self, shape):
-        qmc.QuasiRegistry.clear_registry()
+    def test_quasi_mv(self, shape, clear_registry):
         mv = utils.QuasiMultivariateNormal(torch.zeros(3), torch.eye(3))
         samples = mv.sample(shape)
 
