2f6f0d71730e594f06dc1331fc58cc7e24c6c032
victor <baj>
2022-09-25 21:09:23 +0200

    Fix check

--- pyfilter/inference/batch/mcmc/proposals/symmetric_mh.py
+++ pyfilter/inference/batch/mcmc/proposals/symmetric_mh.py
@@ -1,6 +1,7 @@
 import torch
 from .base import BaseProposal
 from ....utils import construct_mvn
+from ....context import QuasiParameterContext
 
 
 # TODO: Should perhaps create a quasi-specific class for this...?
@@ -13,7 +14,7 @@ class SymmetricMH(BaseProposal):
         values = context.stack_parameters(constrained=False)
         weights = state.normalized_weights()
 
-        return construct_mvn(values, weights, scale=1.1, quasi=context.is_quasi)  # Same scale in in num_particles
+        return construct_mvn(values, weights, scale=1.1, quasi=isinstance(context, QuasiParameterContext))
 
     def exchange(self, latest, candidate, mask: torch.Tensor) -> None:
         return
