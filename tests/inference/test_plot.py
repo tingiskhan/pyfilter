05f234be3ee053f29b64ffc3268efc014bbfbf53
Victor <victor.tingstrom@gmail.com>
2022-12-08 14:59:46 +0100

    Fixes

--- tests/inference/test_plot.py
+++ tests/inference/test_plot.py
@@ -9,11 +9,11 @@ class TestPlotting(object):
         batch_shape = torch.Size([1_000])
         with inf.make_context() as context:
             context.set_batch_shape(batch_shape)
-            alpha = context.named_parameter("alpha", inf.Prior(Normal, loc=0.0, scale=1.0))
-            beta = context.named_parameter("beta", inf.Prior(LogNormal, loc=0.0, scale=1.0))
+            alpha = context.named_parameter("alpha", Normal(loc=0.0, scale=1.0))
+            beta = context.named_parameter("beta", LogNormal(loc=0.0, scale=1.0))
 
             sigma = context.named_parameter(
-                "sigma", inf.Prior(LogNormal, loc=0.0, scale=1.0).expand(torch.Size([2])).to_event(1)
+                "sigma", LogNormal(loc=0.0, scale=1.0).expand(torch.Size([2])).to_event(1)
             )
 
         state = inf.sequential.state.SequentialAlgorithmState(torch.zeros(batch_shape), None)
