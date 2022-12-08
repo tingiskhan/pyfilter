1c0ffbe806304160e810c7c8590490f0b678eb75
Victor <victor.tingstrom@gmail.com>
2022-12-08 19:28:13 +0100

    Adds test for monkey patch

--- tests/inference/test_prior.py
+++ tests/inference/test_prior.py
@@ -1,6 +1,11 @@
-from pyro.distributions import Normal
+from subprocess import call
+from pyro.distributions import Distribution
 
 
 class TestPrior(object):
-    def test_copy_prior(self):
-        pass
+    # TODO: Should verify that all classes have...
+    def test_verify_is_patched(self):
+        from pyfilter.inference import PriorMixin
+
+        for method_name in (mn for mn in dir(PriorMixin) if callable(mn)):
+            assert hasattr(Distribution, method_name)
