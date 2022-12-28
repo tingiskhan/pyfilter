8e5f2cece2b6300dd4b8204fbe9c67d2424af7de
Victor <victor.tingstrom@gmail.com>
2022-12-28 12:02:17 +0100

    Fixes GPF

--- pyfilter/filters/particle/gpf.py
+++ pyfilter/filters/particle/gpf.py
@@ -6,12 +6,13 @@ from .state import ParticleFilterPrediction, ParticleFilterState
 from .utils import log_likelihood
 
 
+# TODO: Add kernels
 class GaussianPF(ParticleFilter):
     """
     Implements the `Gaussian Particle Filter`_ of J.H. Kotecha and P.M. Djuric. 
 
     .. _`Gaussian Particle Filter`: https://ieeexplore.ieee.org/document/1232326
-    """
+    """     
 
     def _generate_importance_density(self, state: ParticleFilterState):
         dim = -(self.ssm.hidden.n_dim + 1)
@@ -21,7 +22,7 @@ class GaussianPF(ParticleFilter):
         if self.ssm.hidden.n_dim == 0:
             dist = Normal(mean, var)
         else:
-            dist = MultivariateNormal(mean, var)
+            dist = MultivariateNormal(mean, covariance_matrix=var)
         
         return dist.expand(self.particles)
 
@@ -29,18 +30,13 @@ class GaussianPF(ParticleFilter):
         return ParticleFilterPrediction(state.x, torch.zeros_like(state.w), state.prev_inds)
 
     def correct(self, y: torch.Tensor, state: ParticleFilterState, prediction: ParticleFilterPrediction) -> ParticleFilterState:                
-        # TODO: This is not that nice tbh...
-        if (state.x.time_index == 0).all():
-            state = ParticleFilterState(self.ssm.hidden.propagate(state.x), state.w, state.ll, state.prev_inds)
-
-        # TODO: Fix batched as the axes get re-arranged
         density = self._generate_importance_density(state)
         x_vals = density.sample()
 
-        x_copy = prediction.prev_x.copy(values=x_vals)        
-        observation_density = self.ssm.build_density(x_copy)
-
+        x_copy = prediction.prev_x.copy(values=x_vals)
         x_new = self.ssm.hidden.propagate(x_copy)
+
+        observation_density = self.ssm.build_density(x_new)
         w = observation_density.log_prob(y)
 
         return ParticleFilterState(x_new, w, log_likelihood(w), state.prev_inds)
