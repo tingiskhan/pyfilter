b148ea070911aef74f10b408dacc5ad5b078679a
Victor <victor.tingstrom@gmail.com>
2022-12-30 15:58:22 +0100

    Improves linearized

--- pyfilter/filters/particle/proposals/approximate.py
+++ pyfilter/filters/particle/proposals/approximate.py
@@ -26,6 +26,9 @@ class GaussianProposal(Proposal):
 
         return x_result, w
 
+    def copy(self) -> "Proposal":
+        return GaussianProposal()
+
 
 # TODO: Clean up
 class LinearizedGaussianProposal(GaussianProposal):
@@ -33,29 +36,37 @@ class LinearizedGaussianProposal(GaussianProposal):
     Implements a Gaussian proposal distribution based on a linearized version.
     """
 
-    def __init__(self, num_steps: int = 5, alpha: float = 1e-3):
+    def __init__(self, num_steps: int = 1, alpha: float = 1e-3, use_second_order: bool = True):
         """
         Internal initializer for :class:`LinearizedGaussianProposal`.
 
         Args:
-            num_steps (int, optional): number of steps. Defaults to 5.
-            alpha (float, optional): _description_. Defaults to 1e-3.
+            num_steps (int, optional): number of steps.
+            alpha (float, optional): alpha in gradient descent, only applies if ``use_second_order`` is ``True``.
+            use_second_order (bool, optional): whether to use second order information.
         """
 
         super().__init__(None)
         self._n_steps = num_steps
-        self._use_second_order = True
+        self._use_second_order = use_second_order
         self._alpha = alpha
-        self._is_1d = None
 
     def sample_and_weight(self, y: torch.Tensor, state: ParticleFilterState, predictive_distribution: Distribution) -> Tuple[TimeseriesState, torch.Tensor]:
-        mean_state = state.timeseries_state.copy(values=state.get_mean())
+        unsqueeze_dim = -(self._model.hidden.n_dim + 1)
+        mean_state = state.timeseries_state.copy(values=state.get_mean().unsqueeze(unsqueeze_dim))
         
         mean, std = self._model.hidden.mean_scale(mean_state)
-        std = (state.get_variance() + std.pow(2.0)).sqrt()
+        std = (state.get_variance().unsqueeze(unsqueeze_dim) + std.pow(2.0)).sqrt()
 
         kernel = find_mode_of_distribution(self._model, predictive_distribution, mean_state, mean, std, y, self._n_steps, self._alpha, self._use_second_order)
         
-        x_result = state.timeseries_state.copy(values=kernel.sample(state.timeseries_state.batch_shape))
+        x_result = state.timeseries_state.copy(values=kernel.expand(state.timeseries_state.batch_shape).sample)
 
         return x_result, self._weight_with_kernel(y, predictive_distribution, x_result, kernel)
+
+    def copy(self) -> "Proposal":
+        return LinearizedGaussianProposal(
+            num_steps=self._n_steps,
+            alpha=self._alpha,
+            use_second_order=self._use_second_order
+        )
