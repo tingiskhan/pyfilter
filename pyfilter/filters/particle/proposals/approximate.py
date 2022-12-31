f30c2c2d8e2c12d39998064984086a5c179c74f9
Victor <victor.tingstrom@gmail.com>
2022-12-31 12:55:11 +0100

    TODO and linear kernel

--- pyfilter/filters/particle/proposals/approximate.py
+++ pyfilter/filters/particle/proposals/approximate.py
@@ -1,14 +1,15 @@
 import torch
-from stochproc.timeseries import TimeseriesState
+from stochproc.timeseries import TimeseriesState, AffineProcess
 from typing import Tuple
 
 from pyro.distributions import Distribution
 
 from ..state import ParticleFilterState
 from .base import Proposal
-from .utils import find_mode_of_distribution
+from .utils import find_mode_of_distribution, find_optimal_density
 
 
+# TODO: Clean up? Perhaps move to separate modules for "exact" and approximate
 class GaussianProposal(Proposal):
     """
     Implements a proposal distribution based on a Gaussian approximation.
@@ -30,7 +31,6 @@ class GaussianProposal(Proposal):
         return GaussianProposal()
 
 
-# TODO: Clean up
 class LinearizedGaussianProposal(GaussianProposal):
     """
     Implements a Gaussian proposal distribution based on a linearized version.
@@ -51,16 +51,15 @@ class LinearizedGaussianProposal(GaussianProposal):
         self._use_second_order = use_second_order
         self._alpha = alpha
 
-    def sample_and_weight(self, y: torch.Tensor, state: ParticleFilterState, predictive_distribution: Distribution) -> Tuple[TimeseriesState, torch.Tensor]:
+    def sample_and_weight(self, y, state, predictive_distribution):
         unsqueeze_dim = -(self._model.hidden.n_dim + 1)
         mean_state = state.timeseries_state.copy(values=state.get_mean().unsqueeze(unsqueeze_dim))
         
         mean, std = self._model.hidden.mean_scale(mean_state)
         std = (state.get_variance().unsqueeze(unsqueeze_dim) + std.pow(2.0)).sqrt()
 
-        kernel = find_mode_of_distribution(self._model, predictive_distribution, mean_state, mean, std, y, self._n_steps, self._alpha, self._use_second_order)
-        
-        x_result = state.timeseries_state.copy(values=kernel.expand(state.timeseries_state.batch_shape).sample)
+        kernel = find_mode_of_distribution(self._model, predictive_distribution, mean_state, mean, std, y, self._n_steps, self._alpha, self._use_second_order).expand(state.timeseries_state.batch_shape)        
+        x_result = state.timeseries_state.copy(values=kernel.sample)
 
         return x_result, self._weight_with_kernel(y, predictive_distribution, x_result, kernel)
 
@@ -70,3 +69,68 @@ class LinearizedGaussianProposal(GaussianProposal):
             alpha=self._alpha,
             use_second_order=self._use_second_order
         )
+
+
+class LinearGaussianProposal(GaussianProposal):
+    # TODO: Fix doc
+
+    def __init__(self, a_index: int = 0, b_index: int = None, s_index: int = -1, is_variance: bool = False):
+        """
+        Initializes the :class:`LinearGaussianProposal` class.
+
+        Args:
+            a_index: index of the parameter that constitutes :math:`A` in the observable process, assumes that
+                it's the first one. If you pass ``None`` it is assumed that this corresponds to an identity matrix.
+            b_index: index of the parameter that constitutes :math:`b` in the observable process.
+            s_index: index of the parameter that constitutes :math:`s` in the observable process.
+            is_variance: whether `s_index` parameter corresponds to a variance or standard deviation parameter.
+        """
+
+        super().__init__()
+        self._a_index = a_index
+        self._b_index = b_index
+        self._s_index = s_index
+
+        self._is_variance = is_variance
+
+    def get_offset_and_scale(
+        self, x: TimeseriesState, parameters: Tuple[torch.Tensor, ...]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Standardizes the observation.
+
+        Args:
+            x: previous state.
+            parameters: parameters of the observations process.
+        """
+
+        a_param = parameters[self._a_index]
+        if self._b_index is None:
+            return a_param, torch.tensor(0.0, device=a_param.device)
+
+        return a_param, parameters[self._b_index]
+
+    def set_model(self, model):
+        if not isinstance(model.hidden, AffineProcess):
+            raise ValueError("Model combination not supported!")
+
+        return super().set_model(model)
+
+    def sample_and_weight(self, y, state, predictive_distribution):
+        unsqueeze_dim = -(self._model.hidden.n_dim + 1)
+        mean_state = state.timeseries_state.copy(values=state.get_mean().unsqueeze(unsqueeze_dim))
+
+        mean, scale = self._model.hidden.mean_scale(mean_state)
+        h_var_inv = (scale.pow(2.0) + state.get_variance().unsqueeze(unsqueeze_dim)).pow(-1.0)
+
+        parameters = self._model.parameters
+        a_param, offset = self.get_offset_and_scale(mean_state, parameters)
+        o_var_inv = parameters[self._s_index].pow(-2.0 if not self._is_variance else -1.0)
+
+        kernel = find_optimal_density(y - offset, mean, h_var_inv, o_var_inv, a_param, self._model).expand(state.timeseries_state.batch_shape)
+        x_result = state.timeseries_state.propagate_from(values=kernel.sample)
+
+        return x_result, self._weight_with_kernel(y, predictive_distribution, x_result, kernel)
+
+    def copy(self) -> "Proposal":
+        return LinearGaussianProposal(self._a_index, self._b_index, self._s_index, self._is_variance)
