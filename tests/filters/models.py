76dd499681f5eaab67629d4c35aaf580ca624cd6
Victor <victor.tingstrom@gmail.com>
2023-01-16 11:44:28 +0100

    Cleanup

--- tests/filters/models.py
+++ tests/filters/models.py
@@ -7,23 +7,8 @@ from pyro.distributions import Normal
 from pykalman import KalmanFilter
 
 
-def build_0d_dist(x, a, s):
-    return Normal(loc=a * x.value, scale=s)
-
-
-def build_1d_dist(x, a, s):
-    return build_2d_to_1d_dist(x, a, s).to_event(1)
-
-
-def build_2d_to_1d_dist(x, a, s):
-    return Normal(loc=a.matmul(x.value.unsqueeze(-1)).squeeze(-1), scale=s)
-
-
-def build_joint(x, a, s):
-    return build_2d_to_1d_dist(x, a, s).to_event(1)
-
-
 def linear_models():
+    # One dimensional
     alpha, beta, sigma = 0.0, 0.99, 0.05
     ar = ts.models.AR(alpha, beta, sigma)
 
@@ -41,7 +26,8 @@ def linear_models():
     )
 
     yield obs_1d_1d, kalman_1d_1d
-
+    
+    # Two dimensional
     sigma = np.array([0.05, 0.1])
     a, s = np.eye(2), 0.15 * np.ones(2)
 
@@ -49,7 +35,7 @@ def linear_models():
 
     parameters = (torch.from_numpy(a).float(), torch.from_numpy(sigma).float())
     rw = ts.LinearModel(
-        parameters, inc_dist, lambda *args: Normal(0.0, 1.0).expand(torch.Size([2])).to_event(1)
+        parameters, inc_dist, lambda m_, s_: Normal(0.0, s_).expand(torch.Size([2])).to_event(1)
     )
 
     params = torch.from_numpy(a).float(), torch.from_numpy(s).float()
@@ -61,27 +47,17 @@ def linear_models():
         observation_matrices=a,
         transition_covariance=state_covariance,
         observation_covariance=s ** 2.0 * np.eye(2),
+        initial_state_covariance=state_covariance
     )
 
     yield obs_2d_2d, kalman_2d_2d
 
-    sigma = 0.05
-    proc_1 = ts.models.RandomWalk(sigma)
-    proc_2 = ts.models.RandomWalk(sigma)
+    # Two dimensional joint equivalent to above
+    proc_1 = ts.models.RandomWalk(parameters[-1][0])
+    proc_2 = ts.models.RandomWalk(parameters[-1][1])
 
     joint = ts.joint_process(proc_1=proc_1, proc_2=proc_2)
-
-    eye = torch.eye(2)
-    joint_ssm = ts.LinearStateSpaceModel(joint, (eye, params[-1]), torch.Size([2]))
-
-    state_covariance = sigma ** 2.0 * np.eye(2)
-    kalman_2d_2d = KalmanFilter(
-        transition_matrices=eye.numpy(),
-        observation_matrices=eye.numpy(),
-        transition_covariance=state_covariance,
-        observation_covariance=s ** 2.0 * np.eye(2),
-        initial_state_covariance=state_covariance
-    )
+    joint_ssm = ts.LinearStateSpaceModel(joint, params, torch.Size([2]))
 
     yield joint_ssm, kalman_2d_2d
 
