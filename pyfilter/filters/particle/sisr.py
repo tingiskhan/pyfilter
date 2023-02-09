44c8e876559e8d1b3558ddfdaff66347ef5bbbde
Victor <victor.tingstrom@gmail.com>
2023-02-09 12:40:27 +0100

    Bug fix

--- pyfilter/filters/particle/sisr.py
+++ pyfilter/filters/particle/sisr.py
@@ -21,16 +21,21 @@ class SISR(ParticleFilter):
         resampled_indices = self._resampler(normalized_weigths[..., mask], normalized=True)
 
         # Resample
-        all_indices = state.previous_indices.masked_scatter(mask.unsqueeze(0), resampled_indices)
-
-        temp_resampled = state.timeseries_state.value[resampled_indices, mask]
-        resampled_x = state.timeseries_state.value.masked_scatter(mask.unsqueeze(0), temp_resampled)
-        resampled_state = state.timeseries_state.copy(values=resampled_x)
-
         unsqueezed_mask = mask.unsqueeze(0)
+        all_indices = state.previous_indices.masked_scatter(unsqueezed_mask, resampled_indices)
+        
         weights = state.weights.masked_fill(unsqueezed_mask, 0.0)
         normalized_weigths = normalized_weigths.masked_fill(unsqueezed_mask, 1.0 / weights.shape[0])
+        
+        timeseries_state = state.timeseries_state.value
+        temp_resampled = timeseries_state[resampled_indices, mask]                
 
+        if self._model.n_dim > 0:
+            unsqueezed_mask.unsqueeze_(-1)
+
+        resampled_x = timeseries_state.masked_scatter(unsqueezed_mask, temp_resampled)
+        resampled_state = state.timeseries_state.copy(values=resampled_x)
+                
         return ParticleFilterPrediction(resampled_state, weights, normalized_weigths, indices=all_indices)
 
     def correct(self, y, prediction):
