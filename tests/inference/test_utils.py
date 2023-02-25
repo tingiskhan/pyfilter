e28d9f516c60bd19e145519ef1a17128985da26f
Victor <victor.tingstrom@gmail.com>
2023-02-25 07:22:28 +0100

    Test fix

--- tests/inference/test_utils.py
+++ tests/inference/test_utils.py
@@ -1,22 +1,19 @@
+from random import random
 import pytest
 import torch
 
 from pyfilter.inference import utils, qmc
 
 BATCH_SHAPES = [torch.Size([]), torch.Size([512, 1]), torch.Size([50, 2, 3])]
-KEY = 123
-
-@pytest.fixture(params=[True, False])
-def clear_registry(request):
-    qmc.QuasiRegistry.add_engine(KEY, 3, request.param)
-    yield
-    qmc.QuasiRegistry.clear_registry()
 
 
 class TestUtils(object):
     @pytest.mark.parametrize("shape", BATCH_SHAPES)
-    def test_quasi_mv(self, shape, clear_registry):
-        mv = utils.QuasiMultivariateNormal(KEY, torch.zeros(3), torch.eye(3))
+    @pytest.mark.parametrize("randomize", [True, False])
+    def test_quasi_mv(self, shape, randomize):
+        dim = 3
+        engine = qmc.EngineContainer(dim, randomize=randomize)
+        mv = utils.QuasiMultivariateNormal(engine, torch.zeros(dim), torch.eye(dim))
         samples = mv.sample(shape)
 
         assert (samples.shape == shape + mv.event_shape)
